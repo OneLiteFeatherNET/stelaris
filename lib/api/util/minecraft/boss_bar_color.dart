46b517de99e34cf4744f6de23fcd62c2a4030f16
theEvilReaper <steffenwx@gmail.com>
2022-09-07 23:07:55 +0200

    Add more minecraft related stuff

---
+++ lib/api/util/minecraft/boss_bar_color.dart
@@ -0,0 +1,14 @@
+enum BossBarColor {
+
+  pint("pink"),
+  blue("blue"),
+  red("red"),
+  green("green"),
+  yellow("yellow"),
+  purple("purple"),
+  white("white");
+
+  final String name;
+
+  const BossBarColor(this.name);
+}
