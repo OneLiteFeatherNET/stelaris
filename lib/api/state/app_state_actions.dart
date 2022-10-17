6e5bc27e96b85ea6b464fe2b946eac2ede5afec0
Joltra <MinnyMouseXDmail.com>
2022-10-17 20:49:32 +0200

    Update update actions

--- lib/api/state/app_state_actions.dart
+++ lib/api/state/app_state_actions.dart
@@ -1,36 +1,71 @@
+import 'dart:async';
+
+import 'package:async_redux/async_redux.dart';
+import 'package:stelaris_ui/api/api_service.dart';
 import 'package:stelaris_ui/api/model/notification_model.dart';
+import 'package:stelaris_ui/api/state/app_state.dart';
 
 import '../model/block_model.dart';
 import '../model/font_model.dart';
 import '../model/item_model.dart';
 import '../model/plugin_model.dart';
 
-class UpdateItemAction {
+class UpdateItemAction extends ReduxAction<AppState> {
   final ItemModel item;
 
   UpdateItemAction(this.item);
+
+  @override
+  Future<AppState?> reduce() async {
+    var items = await ApiService().itemApi.getAllItems();
+    return state.copyWith(items: items);
+  }
 }
 
-class UpdateNotificationAction {
+class UpdateNotificationAction extends ReduxAction<AppState> {
   final NotificationModel notification;
 
   UpdateNotificationAction(this.notification);
+
+  @override
+  Future<AppState?> reduce() async {
+    var notifications = await ApiService().notificationAPI.getAllNotifications();
+    return state.copyWith(notifications: notifications);
+  }
 }
 
-class UpdateFontAction {
+class UpdateFontAction extends ReduxAction<AppState> {
   final FontModel font;
 
   UpdateFontAction(this.font);
+
+  @override
+  Future<AppState?> reduce() async {
+    var fonts = await ApiService().fontAPI.getAllFonts();
+    return state.copyWith(fonts: fonts);
+  }
 }
 
-class UpdatePluginAction {
+class UpdatePluginAction extends ReduxAction<AppState> {
   final PluginModel plugin;
 
   UpdatePluginAction(this.plugin);
+
+  @override
+  Future<AppState?> reduce() async {
+    var plugins = await ApiService().pluginAPI.getAllPlugins();
+    return state.copyWith(plugins: plugins);
+  }
 }
 
-class UpdateBlockAction {
+class UpdateBlockAction extends ReduxAction<AppState> {
   final BlockModel block;
 
   UpdateBlockAction(this.block);
+
+  @override
+  Future<AppState?> reduce() async {
+    var blocks = await ApiService().blockAPI.getAllBlocks();
+    return state.copyWith(blocks: blocks);
+  }
 }
