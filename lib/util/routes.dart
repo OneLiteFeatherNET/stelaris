2263ae9f705c2d8b2b2489685a8fc4c182a053d2
theEvilReaper <steffenwx@gmail.com>
2022-10-24 12:04:15 +0200

    Add new routes

--- lib/util/routes.dart
+++ lib/util/routes.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:stelaris_ui/api/util/navigation.dart';
 import 'package:stelaris_ui/feature/landing/landing_page.dart';
 import 'package:stelaris_ui/feature/login/login_page.dart';
 
@@ -14,6 +15,39 @@ final GoRouter router = GoRouter(routes: [
   ),
   GoRoute(
     path: '/login',
+    pageBuilder: (context, state) => CustomTransitionPage(
+        child: const LoginPage(),
+        key: state.pageKey,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+            FadeTransition(
+              opacity: animation,
+              child: child,
+            )),
+  ),
+  GoRoute(
+    path: NavigationEntry.items.route,
+    pageBuilder: (context, state) => CustomTransitionPage(
+        child: const LoginPage(),
+        key: state.pageKey,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+            FadeTransition(
+              opacity: animation,
+              child: child,
+            )),
+  ),
+  GoRoute(
+    path: NavigationEntry.entities.route,
+    pageBuilder: (context, state) => CustomTransitionPage(
+        child: const LoginPage(),
+        key: state.pageKey,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+            FadeTransition(
+              opacity: animation,
+              child: child,
+            )),
+  ),
+  GoRoute(
+    path: NavigationEntry.build.route,
     pageBuilder: (context, state) => CustomTransitionPage(
         child: const LoginPage(),
         key: state.pageKey,
