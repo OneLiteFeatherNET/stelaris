524769d579194a75f849c9e2f60051aecc6f32a6
theEvilReaper <steffenwx@gmail.com>
2022-10-24 23:51:25 +0200

    Change to BasePage

--- lib/util/routes.dart
+++ lib/util/routes.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:stelaris_ui/api/util/navigation.dart';
-import 'package:stelaris_ui/feature/landing/landing_page.dart';
+import 'package:stelaris_ui/feature/base/base_page.dart';
 import 'package:stelaris_ui/feature/login/login_page.dart';
 
 final GoRouter router = GoRouter(routes: [
   GoRoute(
     path: '/',
     builder: (BuildContext context, GoRouterState state) {
-      return const LandingPage(
-        child: LoginPage(),
-      );
+      return const BasePage(child: LoginPage());
     },
   ),
   GoRoute(
@@ -27,7 +25,7 @@ final GoRouter router = GoRouter(routes: [
   GoRoute(
     path: NavigationEntry.items.route,
     pageBuilder: (context, state) => CustomTransitionPage(
-        child: const LoginPage(),
+        child: const BasePage(child: LoginPage()),
         key: state.pageKey,
         transitionsBuilder: (context, animation, secondaryAnimation, child) =>
             FadeTransition(
