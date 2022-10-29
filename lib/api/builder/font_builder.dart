c27828a1fa0c8f77f3d4b8d5a42806530d90b509
theEvilReaper <steffenwx@gmail.com>
2022-10-29 20:37:45 +0200

    Add const variable for the generatorKey

--- lib/api/builder/font_builder.dart
+++ lib/api/builder/font_builder.dart
@@ -3,8 +3,10 @@ import 'package:stelaris_ui/api/model/font_model.dart';
 
 class FontBuilder extends BaseBuilder<FontModel> {
 
+  static const String _generatorKey = "FontGenerator";
+
   @override
   FontModel toDTO() {
-    return FontModel(name: name, generator: generator);
+    return FontModel(name: name, generator: _generatorKey);
   }
 }
