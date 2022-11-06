798bec6152ea8ff30cfdf15547372a45e0b01316
theEvilReaper <steffenwx@gmail.com>
2022-11-06 16:56:43 +0100

    Add new enum entries and add toColumn method

--- lib/feature/plugin/plugin_columns.dart
+++ lib/feature/plugin/plugin_columns.dart
@@ -1,10 +1,22 @@
+import 'package:data_table_2/data_table_2.dart';
+import 'package:flutter/material.dart';
+
 enum PluginColumns {
 
   name("Name"),
+  description("Description"),
   version("Version"),
-  ref("Ref");
+  ref("Ref"),
+  actions("Actions");
 
   final String display;
 
   const PluginColumns(this.display);
+
+  DataColumn2 toColumn() {
+    return DataColumn2(
+        label: Text(display, textAlign: TextAlign.center),
+        size: ColumnSize.L
+    );
+  }
 }
