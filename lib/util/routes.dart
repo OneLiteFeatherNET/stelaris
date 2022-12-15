2e56e3f22f8aa90fcdfe37b48c31c24220522f4f
theEvilReaper <steffenwx@gmail.com>
2022-12-15 23:14:12 +0100

    Add and update routes

--- lib/util/routes.dart
+++ lib/util/routes.dart
@@ -3,6 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:stelaris_ui/api/util/navigation.dart';
 import 'package:stelaris_ui/feature/base/base_page.dart';
 import 'package:stelaris_ui/feature/item/item_page.dart';
+import 'package:stelaris_ui/feature/notification/notification_page.dart';
 
 import '../feature/login/login_page.dart';
 
@@ -10,7 +11,7 @@ final GoRouter router = GoRouter(routes: [
   GoRoute(
     path: '/',
     builder: (BuildContext context, GoRouterState state) {
-      return const BasePage(child: ItemPage());
+      return const BasePage(child: NotificationPage());
     },
   ),
   GoRoute(
@@ -27,7 +28,18 @@ final GoRouter router = GoRouter(routes: [
   GoRoute(
     path: NavigationEntry.items.route,
     pageBuilder: (context, state) => CustomTransitionPage(
-        child: const BasePage(child: LoginPage()),
+        child: const BasePage(child: ItemPage()),
+        key: state.pageKey,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+            FadeTransition(
+              opacity: animation,
+              child: child,
+            )),
+  ),
+  GoRoute(
+    path: NavigationEntry.dashboard.route,
+    pageBuilder: (context, state) => CustomTransitionPage(
+        child:  BasePage(child: Container()),
         key: state.pageKey,
         transitionsBuilder: (context, animation, secondaryAnimation, child) =>
             FadeTransition(
@@ -38,7 +50,7 @@ final GoRouter router = GoRouter(routes: [
   GoRoute(
     path: NavigationEntry.entities.route,
     pageBuilder: (context, state) => CustomTransitionPage(
-        child: const LoginPage(),
+        child:  BasePage(child: Container()),
         key: state.pageKey,
         transitionsBuilder: (context, animation, secondaryAnimation, child) =>
             FadeTransition(
@@ -49,7 +61,18 @@ final GoRouter router = GoRouter(routes: [
   GoRoute(
     path: NavigationEntry.build.route,
     pageBuilder: (context, state) => CustomTransitionPage(
-        child: const LoginPage(),
+        child:  BasePage(child: Container()),
+        key: state.pageKey,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+            FadeTransition(
+              opacity: animation,
+              child: child,
+            )),
+  ),
+  GoRoute(
+    path: NavigationEntry.notifications.route,
+    pageBuilder: (context, state) => CustomTransitionPage(
+        child:  const BasePage(child: NotificationPage()),
         key: state.pageKey,
         transitionsBuilder: (context, animation, secondaryAnimation, child) =>
             FadeTransition(
