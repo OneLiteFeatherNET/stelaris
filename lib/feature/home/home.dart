3defd100459b8a0bf72c9582a37fb532936777b5
theEvilReaper <steffenwx@gmail.com>
2022-12-15 23:10:47 +0100

    Add ValueNotifier to switch the themes

--- lib/feature/home/home.dart
+++ lib/feature/home/home.dart
@@ -2,19 +2,27 @@ import 'package:flutter/material.dart';
 import 'package:stelaris_ui/util/routes.dart';
 import 'package:stelaris_ui/util/themes.dart';
 
+final ValueNotifier<ThemeMode> notifier = ValueNotifier(ThemeMode.dark);
+
 class StelarisApp extends StatelessWidget {
+
   const StelarisApp({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationParser: router.routeInformationParser,
-      routerDelegate: router.routerDelegate,
-      routeInformationProvider: router.routeInformationProvider,
-      title: "Stelaris",
-      debugShowCheckedModeBanner: false,
-      theme: darkMode,
-    );
+    // notifier.value = MediaQuery.of(context).platformBrightness == Brightness.dark ? ThemeMode.dark : ThemeMode.light;
+    return ValueListenableBuilder<ThemeMode>(valueListenable: notifier, builder: (_, mode, __) {
+      return  MaterialApp.router(
+        routeInformationParser: router.routeInformationParser,
+        routerDelegate: router.routerDelegate,
+        routeInformationProvider: router.routeInformationProvider,
+        title: "Stelaris",
+        debugShowCheckedModeBanner: false,
+        darkTheme: darkMode,
+        theme: lightMode,
+        themeMode: mode,
+      );
+    });
   }
 }
