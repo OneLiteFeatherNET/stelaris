6462613e79294db9bba3c77c892df2d506ffe533
theEvilReaper <steffenwx@gmail.com>
2022-12-17 11:59:11 +0100

    Disable some unused navigation entries

--- lib/api/util/navigation.dart
+++ lib/api/util/navigation.dart
@@ -3,11 +3,12 @@ import 'package:flutter/material.dart';
 /// The enum class contains all relevant item for the navigation widget.
 enum NavigationEntry {
 
-  dashboard("Dashboard", "/dashboard", Icons.abc_outlined),
-  items("Items", "/item", Icons.abc),
-  entities("Entities", "/entities", Icons.add_moderator),
+  //dashboard("Dashboard", "/dashboard", Icons.abc_outlined),
+  items("Items", "/item", Icons.games),
+  //entities("Entities", "/entities", Icons.add_moderator),
   //quests("Quests", "/quests",Icons.access_time_filled_outlined),
   notifications("Notifications", "/notifications", Icons.notification_add),
+  plugins("Plugins", "/plugins", Icons.extension),
   build("Build", "/build", Icons.construction);
 
   final String display;
