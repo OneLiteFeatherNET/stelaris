f6ac6ee71077ac139f85705c422c7d4ff34cef1b
theEvilReaper <steffenwx@gmail.com>
2022-12-17 13:55:35 +0100

    Update AddButton class

--- lib/feature/base/button/add_button.dart
+++ lib/feature/base/button/add_button.dart
@@ -1,25 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:stelaris_ui/util/constants.dart';
 
 class AddButton extends StatelessWidget {
 
-  final Icon? icon;
   final VoidCallback openFunction;
 
-  const AddButton({Key? key, required this.openFunction, this.icon}) : super(key: key);
+  const AddButton({Key? key, required this.openFunction}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 50,
-      width: 50,
-      child: FloatingActionButton(
-        autofocus: false,
+    return FloatingActionButton.extended(
         onPressed: openFunction,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-        child: icon,
-      ),
+        label: addText,
+        icon: const Icon(Icons.add),
     );
   }
 }
