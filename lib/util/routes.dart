b12cc1b6ac88cdec5991d2e02beafbfe0f6b48e5
theEvilReaper <steffenwx@gmail.com>
2022-12-17 12:02:57 +0100

    Updates routes

--- lib/util/routes.dart
+++ lib/util/routes.dart
@@ -4,6 +4,7 @@ import 'package:stelaris_ui/api/util/navigation.dart';
 import 'package:stelaris_ui/feature/base/base_page.dart';
 import 'package:stelaris_ui/feature/item/item_page.dart';
 import 'package:stelaris_ui/feature/notification/notification_page.dart';
+import 'package:stelaris_ui/feature/plugin/plugin_list.dart';
 
 import '../feature/login/login_page.dart';
 
@@ -37,28 +38,6 @@ final GoRouter router = GoRouter(routes: [
             )),
   ),
   GoRoute(
-    path: NavigationEntry.dashboard.route,
-    pageBuilder: (context, state) => CustomTransitionPage(
-        child:  BasePage(child: Container()),
-        key: state.pageKey,
-        transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-            FadeTransition(
-              opacity: animation,
-              child: child,
-            )),
-  ),
-  GoRoute(
-    path: NavigationEntry.entities.route,
-    pageBuilder: (context, state) => CustomTransitionPage(
-        child:  BasePage(child: Container()),
-        key: state.pageKey,
-        transitionsBuilder: (context, animation, secondaryAnimation, child) =>
-            FadeTransition(
-              opacity: animation,
-              child: child,
-            )),
-  ),
-  GoRoute(
     path: NavigationEntry.build.route,
     pageBuilder: (context, state) => CustomTransitionPage(
         child:  BasePage(child: Container()),
@@ -73,6 +52,17 @@ final GoRouter router = GoRouter(routes: [
     path: NavigationEntry.notifications.route,
     pageBuilder: (context, state) => CustomTransitionPage(
         child:  const BasePage(child: NotificationPage()),
+        key: state.pageKey,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+            FadeTransition(
+              opacity: animation,
+              child: child,
+            )),
+  ),
+  GoRoute(
+    path: NavigationEntry.plugins.route,
+    pageBuilder: (context, state) => CustomTransitionPage(
+        child:  const BasePage(child: PluginPage()),
         key: state.pageKey,
         transitionsBuilder: (context, animation, secondaryAnimation, child) =>
             FadeTransition(
