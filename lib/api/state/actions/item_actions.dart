97349cf780117820b0127ad4dff9d11458bf3d95
Joltra <MinnyMouseXDmail.com>
2022-12-20 22:52:34 +0100

    Add some new actions for the ItemModel

--- lib/api/state/actions/item_actions.dart
+++ lib/api/state/actions/item_actions.dart
@@ -1,17 +1,21 @@
 import 'package:async_redux/async_redux.dart';
+import 'package:stelaris_ui/util/default.dart';
 
-import '../../api_service.dart';
 import '../../model/item_model.dart';
 import '../app_state.dart';
 
 class UpdateItemAction extends ReduxAction<AppState> {
-  final ItemModel item;
+  final ItemModel oldEntry;
+  final ItemModel newEntry;
 
-  UpdateItemAction(this.item);
+  UpdateItemAction(this.oldEntry, this.newEntry);
+
 
   @override
   Future<AppState?> reduce() async {
-    var items = await ApiService().itemApi.getAllItems();
+    final items = List.of(state.items, growable: true);
+    items.remove(oldEntry);
+    items.add(newEntry);
     return state.copyWith(items: items);
   }
 }
@@ -20,9 +24,37 @@ class InitItemAction extends ReduxAction<AppState> {
 
   @override
   Future<AppState?> reduce() async {
-    var items = await ApiService().itemApi.getAllItems();
-    return state.copyWith(items: items);
+    // var items = await ApiService().itemApi.getAllItems();
+    return state.copyWith(items: [defaultItem]);
   }
 
   InitItemAction();
+}
+
+class AddItemAction extends ReduxAction<AppState> {
+
+  final ItemModel model;
+
+  AddItemAction(this.model);
+
+  @override
+  Future<AppState?> reduce() async {
+    final items = List.of(state.items, growable: true);
+    items.add(model);
+    return state.copyWith(items: items);
+  }
+}
+
+class RemoveItemAction extends ReduxAction<AppState> {
+
+  final ItemModel model;
+
+  RemoveItemAction(this.model);
+
+  @override
+  Future<AppState?> reduce() async {
+    final items = List.of(state.items, growable: true);
+    items.remove(model);
+    return state.copyWith(items: items);
+  }
 }
