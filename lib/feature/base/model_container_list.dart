5da1f3e0b105988a21b87576d16cfd97e5a8c0e5
Joltra <MinnyMouseXDmail.com>
2022-12-20 22:39:46 +0100

    Update generic usage, improve variable passing to the state and add more functions

--- lib/feature/base/model_container_list.dart
+++ lib/feature/base/model_container_list.dart
@@ -1,41 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:stelaris_ui/api/model/data_model.dart';
 import 'package:stelaris_ui/feature/base/model_list.dart';
+import 'package:stelaris_ui/feature/dialogs/dismiss_dialog.dart';
 
 import '../../api/tabs/tab_pages.dart';
 
 var tabPagesValues = TabPages.values;
 
-var tabs = tabPagesValues.map((e) => Tab(child: Text(e.content),)).toList();
+var tabs = tabPagesValues
+    .map((e) => Tab(
+          child: Text(e.content),
+        ))
+    .toList();
 
-typedef TabPageMapFunction<E extends DataModel> = Widget Function(TabPages page, ValueNotifier<E?> notification);
+typedef TabPageMapFunction<E extends DataModel> = Widget Function(
+    TabPages page, ValueNotifier<E?> notification);
 
 class ModelContainerList<E extends DataModel> extends StatefulWidget {
-
   final List<E> items;
-  final TabPageMapFunction page;
+  final TabPageMapFunction<E> page;
   final MapToDataModelItem<E> mapToDataModelItem;
   final VoidCallback openFunction;
+  final ValueNotifier<E?> selectedItem;
+  final MapToDeleteSuccessfully<E> mapToDeleteSuccessfully;
+  final MapToDeleteDialog<E> mapToDeleteDialog;
 
-  const ModelContainerList({Key? key, required this.items, required this.page, required this.mapToDataModelItem, required this.openFunction}) : super(key: key);
+  const ModelContainerList(
+      {Key? key,
+      required this.items,
+      required this.page,
+      required this.mapToDataModelItem,
+      required this.openFunction,
+      required this.selectedItem,
+      required this.mapToDeleteSuccessfully,
+      required this.mapToDeleteDialog})
+      : super(key: key);
 
   @override
-  State<ModelContainerList> createState() => _ModelContainerListState<E>(items, page, mapToDataModelItem);
+  State<ModelContainerList<E>> createState() => _ModelContainerListState<E>();
 }
 
-class _ModelContainerListState<E extends DataModel> extends State<ModelContainerList> {
-
-  final List<E> items;
-  final TabPageMapFunction tabPages;
-  final MapToDataModelItem<E> mapToDataModelItem;
-  final ValueNotifier<E?> selectedItem = ValueNotifier(null);
-
-  _ModelContainerListState(this.items, this.tabPages, this.mapToDataModelItem);
-
+class _ModelContainerListState<E extends DataModel>
+    extends State<ModelContainerList<E>> {
   @override
   Widget build(BuildContext context) {
-    return  Row(children: [
-      ModelList(items: items, mapToDataModelItem: mapToDataModelItem, selectedItem: selectedItem, openFunction: widget.openFunction),
+    return Row(children: [
+      ModelList(
+          items: widget.items,
+          mapToDataModelItem: widget.mapToDataModelItem,
+          selectedItem: widget.selectedItem,
+          openFunction: widget.openFunction,
+          mapToDeleteDialog: widget.mapToDeleteDialog,
+          mapToDeleteSuccessfully: widget.mapToDeleteSuccessfully),
       DefaultTabController(
           length: tabPagesValues.length,
           child: Expanded(
@@ -47,7 +63,9 @@ class _ModelContainerListState<E extends DataModel> extends State<ModelContainer
                 ),
               ),
               body: TabBarView(
-                children: tabPagesValues.map((e) => tabPages(e, selectedItem)).toList(),
+                children: tabPagesValues
+                    .map((e) => widget.page(e, widget.selectedItem))
+                    .toList(),
               ),
             ),
           ))
