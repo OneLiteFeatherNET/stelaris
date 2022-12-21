26c448741e7d5279f1194bbce45d725cceabf66b
theEvilReaper <steffenwx@gmail.com>
2022-12-21 23:24:36 +0100

    Add route for FontPage

--- lib/util/routes.dart
+++ lib/util/routes.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:stelaris_ui/api/util/navigation.dart';
 import 'package:stelaris_ui/feature/base/base_page.dart';
+import 'package:stelaris_ui/feature/font/font_page.dart';
 import 'package:stelaris_ui/feature/item/item_page.dart';
 import 'package:stelaris_ui/feature/notification/notification_page.dart';
 import 'package:stelaris_ui/feature/plugin/plugin_list.dart';
@@ -39,6 +40,18 @@ final GoRouter router = GoRouter(routes: [
     path: NavigationEntry.notifications.route,
     pageBuilder: (context, state) => CustomTransitionPage(
         child:  const BasePage(child: NotificationPage()),
+        key: state.pageKey,
+        transitionsBuilder: (context, animation, secondaryAnimation, child) =>
+            FadeTransition(
+              opacity: animation,
+              child: child,
+            )
+    ),
+  ),
+  GoRoute(
+    path: NavigationEntry.font.route,
+    pageBuilder: (context, state) => CustomTransitionPage(
+        child: const BasePage(child: FontPage()),
         key: state.pageKey,
         transitionsBuilder: (context, animation, secondaryAnimation, child) =>
             FadeTransition(
