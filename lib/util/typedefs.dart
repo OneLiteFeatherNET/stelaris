818e3388678a98090ff9f03bd39933dd6f797839
theEvilReaper <steffenwx@gmail.com>
2022-12-21 22:47:55 +0100

    Add new typedef definitions

--- lib/util/typedefs.dart
+++ lib/util/typedefs.dart
@@ -1,4 +1,16 @@
+import 'package:stelaris_ui/api/model/data_model.dart';
+import 'package:stelaris_ui/api/util/minecraft/enchantment.dart';
 
-///Defines all typedef function for the card which displays data from a model
+/// Defines all typedef function for the card which displays data from a model
 typedef ValueUpdate<E> = void Function(E? value);
 typedef DefaultValue<E,T> = E Function(T value);
+
+/// Defines all typedef functions for the SetupStepper
+typedef FinishStepper<E extends DataModel> = void Function(E model);
+typedef BuildModel<E extends DataModel> = E Function(String name, String description);
+
+/// DismissDialog functions
+typedef MapToDeleteSuccessfully<E> = bool Function(E value);
+
+/// Enchantmentdialog functions
+typedef AddEnchantmentCallback = void Function(Enchantment selected, int level);
