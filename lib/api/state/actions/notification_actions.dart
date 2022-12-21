06c256ebbd63825e4a4703203e2e760f62c39e53
theEvilReaper <steffenwx@gmail.com>
2022-12-21 21:38:36 +0100

    Add new actions

--- lib/api/state/actions/notification_actions.dart
+++ lib/api/state/actions/notification_actions.dart
@@ -1,8 +1,21 @@
 import 'package:async_redux/async_redux.dart';
+import 'package:stelaris_ui/util/default.dart';
 
 import '../../api_service.dart';
 import '../../model/notification_model.dart';
 import '../app_state.dart';
+
+class InitNotificationAction extends ReduxAction<AppState> {
+
+  @override
+  Future<AppState?> reduce() async {
+    // var items = await ApiService().itemApi.getAllItems();
+    return state.copyWith(notifications: [firstNotificationModel, secondNotificationModel]);
+  }
+
+  InitNotificationAction();
+}
+
 
 class UpdateNotificationAction extends ReduxAction<AppState> {
   final NotificationModel notification;
@@ -25,4 +38,18 @@ class InputNotificationAction extends ReduxAction<AppState> {
   }
 
   InputNotificationAction();
+}
+
+class RemoveNotificationAction extends ReduxAction<AppState> {
+
+  final NotificationModel model;
+
+  RemoveNotificationAction(this.model);
+
+  @override
+  Future<AppState?> reduce() async {
+    final notifications = List.of(state.notifications, growable: true);
+    notifications.remove(model);
+    return state.copyWith(notifications: notifications);
+  }
 }
