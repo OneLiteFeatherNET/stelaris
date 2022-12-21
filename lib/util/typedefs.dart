3a0af53668aa4a340d7b76cd4dec2d4b7ae13e58
theEvilReaper <steffenwx@gmail.com>
2022-12-21 22:52:13 +0100

    Add definitions from some other classes

--- lib/util/typedefs.dart
+++ lib/util/typedefs.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:stelaris_ui/api/model/data_model.dart';
+import 'package:stelaris_ui/api/tabs/tab_pages.dart';
 import 'package:stelaris_ui/api/util/minecraft/enchantment.dart';
 
 /// Defines all typedef function for the card which displays data from a model
@@ -14,3 +16,12 @@ typedef MapToDeleteSuccessfully<E> = bool Function(E value);
 
 /// Enchantmentdialog functions
 typedef AddEnchantmentCallback = void Function(Enchantment selected, int level);
+
+
+/// ModeList functions
+typedef MapToDataModelItem<E extends DataModel> = Widget Function(E value);
+typedef MapToDeleteDialog<E extends DataModel> = List<TextSpan> Function(E value);
+
+/// ModelContainerList functions
+typedef TabPageMapFunction<E extends DataModel> = Widget Function(
+    TabPages page, ValueNotifier<E?> notification);
