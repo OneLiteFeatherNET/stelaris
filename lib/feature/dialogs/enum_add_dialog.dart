b50a4761be9152f110a582bdaa1fd37f09c7104e
theEvilReaper <steffenwx@gmail.com>
2022-12-23 23:40:00 +0100

    Add dialog to select a enum value

---
+++ lib/feature/dialogs/enum_add_dialog.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:stelaris_ui/util/typedefs.dart';
+
+class EnumAddDialog<E> extends StatefulWidget {
+
+  final Text title;
+  final List<DropdownMenuItem<E>> items;
+  final ValueUpdate<E> valueUpdate;
+
+  const EnumAddDialog({Key? key, required this.title, required this.items, required this.valueUpdate}) : super(key: key);
+
+  @override
+  State<EnumAddDialog<E>> createState() => _EnumAddDialogState<E>();
+}
+
+class _EnumAddDialogState<E> extends State<EnumAddDialog<E>> {
+
+  E? defaultValue;
+
+  @override
+  Widget build(BuildContext context) {
+    defaultValue = widget.items.first.value;
+    return SimpleDialog(
+      title: widget.title,
+      contentPadding: const EdgeInsets.all(20.0),
+      children: [
+        SizedBox(
+            width: 300,
+            child: DropdownButtonFormField<E>(
+              items: widget.items,
+              value: defaultValue,
+              onChanged: (E? value) {
+                if (value == null) return;
+                defaultValue = value;
+              },
+            )
+        ),
+        const SizedBox(height: 25),
+        TextButton(onPressed: () {
+          widget.valueUpdate(defaultValue);
+        }, child: const Text("Add"))
+      ],
+    );
+  }
+}
