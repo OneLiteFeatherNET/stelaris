aa26956771f838a03675ab3e7edda3b97f23f08b
theEvilReaper <steffenwx@gmail.com>
2022-12-23 23:38:17 +0100

    Update some values

--- lib/util/default.dart
+++ lib/util/default.dart
@@ -8,9 +8,9 @@ import 'package:stelaris_ui/api/util/minecraft/frame_type.dart';
 var defaultItem = const ItemModel(
     name: "Test1234",
     generator: "ItemGenerator",
-    group: "Test",
+    //group: "Test",
     material: "minecraft:air",
-    modelData: 1,
+    customModelId: 1,
     amount: 1,
     flags: {"YOLO", "YOLO2", "YOLO3"},
     enchantments: {'yolo': 1, "yolo2": 2, "yolor3": 33},
@@ -44,4 +44,6 @@ var fontModel = FontModel(
     name: "Font",
     generator: "FontGenerator",
     type: FontType.bitmap.displayName,
+    ascent: 10,
+    height: 100
 );
