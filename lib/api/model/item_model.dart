2888eb9e6427010dfbea67dc5277a7ecfb1533aa
theEvilReaper <steffenwx@gmail.com>
2022-12-23 23:40:52 +0100

    Add new values to the ItemModel

--- lib/api/model/item_model.dart
+++ lib/api/model/item_model.dart
@@ -9,11 +9,13 @@ class ItemModel extends DataModel with _$ItemModel {
 
   const factory ItemModel({
     @JsonKey(name: 'name') String? name,
-    @JsonKey(name: "group") String? group,
-    @JsonKey(name: "generator") String? generator,
+    @JsonKey(name: 'description') String? description,
+    @JsonKey(name: 'displayName') String? displayName,
+    @JsonKey(name: 'group') String? group,
+    @JsonKey(name: "generator", defaultValue: "ItemGenerator") String? generator,
     @JsonKey(name: 'material') String? material,
-    @JsonKey(name: 'modelData') int? modelData,
-    @JsonKey(name: 'amount') int? amount,
+    @JsonKey(name: 'customModelId') int? customModelId,
+    @JsonKey(name: 'amount', defaultValue: 1) int? amount,
     @JsonKey(name: 'enchantments') Map<String, int>? enchantments,
     @JsonKey(name: 'itemFlags') Set<String>? flags,
     @JsonKey(name: 'lore') List<String>? lore,
