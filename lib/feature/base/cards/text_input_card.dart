15a3aa5ee91cdd6e0974e4f4a2ebd2e651428245
theEvilReaper <steffenwx@gmail.com>
2023-01-02 10:54:07 +0100

    The TextInputCard widget is now a StatelessWidget

--- lib/feature/base/cards/text_input_card.dart
+++ lib/feature/base/cards/text_input_card.dart
@@ -3,38 +3,25 @@ import 'package:flutter/services.dart';
 import 'package:stelaris_ui/feature/base/base_layout.dart';
 import 'package:stelaris_ui/util/typedefs.dart';
 
-class TextInputCard<E> extends StatefulWidget {
+class TextInputCard<E> extends StatelessWidget with BaseLayout {
 
   final Text title;
   final ValueUpdate<dynamic> valueUpdate;
   final String currentValue;
   final TextInputType? inputType;
   final List<TextInputFormatter>? formatter;
-
-  const TextInputCard({Key? key, required this.title, required this.currentValue, required this.valueUpdate, this.inputType, this.formatter}) : super(key: key);
-
-  @override
-  State<TextInputCard> createState() => _TextInputCardState();
-}
-
-class _TextInputCardState extends State<TextInputCard> with BaseLayout {
-
   final TextEditingController _editController = TextEditingController();
 
-  @override
-  void dispose() {
-    _editController.dispose();
-    super.dispose();
-  }
+  TextInputCard({Key? key, required this.title, required this.currentValue, required this.valueUpdate, this.inputType, this.formatter}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    _editController.text = widget.currentValue;
+    _editController.text = currentValue;
     return Padding(
       padding: padding,
       child: constructContainer(
         [
-          widget.title,
+          title,
           spaceBox,
           SizedBox(
             width: 300,
@@ -43,12 +30,12 @@ class _TextInputCardState extends State<TextInputCard> with BaseLayout {
               child: TextFormField(
                 autocorrect: false,
                 controller: _editController,
-                keyboardType: widget.inputType,
-                inputFormatters: widget.formatter,
+                keyboardType: inputType,
+                inputFormatters: formatter,
               ),
               onFocusChange: (focus) {
                 if (!focus) {
-                  widget.valueUpdate(_editController.value.text);
+                  valueUpdate(_editController.value.text);
                 }
               },
             ),
