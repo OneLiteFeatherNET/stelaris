f2f4fcb25b6d25b1702a66b7aaf1f89bc1960aa6
theEvilReaper <steffenwx@gmail.com>
2023-01-15 22:38:14 +0100

    Improve widget tree

--- lib/feature/build/build_page.dart
+++ lib/feature/build/build_page.dart
@@ -1,11 +1,11 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
-import 'package:stelaris_ui/api/api_service.dart';
 import 'package:stelaris_ui/api/state/app_state.dart';
-import 'package:stelaris_ui/feature/build/dialog/download_dialog.dart';
+import 'package:stelaris_ui/feature/base/cards/expandable_data_card.dart';
+import 'package:stelaris_ui/feature/build/dialog/version_dialog.dart';
+
 
 class BuildPage extends StatefulWidget {
-
   const BuildPage({Key? key}) : super(key: key);
 
   @override
@@ -16,40 +16,39 @@ class BuildPageState extends State<BuildPage> {
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, AppState>(
-        onInit: (store) {},
-        converter: (store) {
-          return store.state;
-        },
-        builder: (context, vm) {
-          return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    TextButton(
-                        onPressed: () {},
-                        child: const Text("Build")
-                    ),
-                    TextButton(
-                        onPressed: () {
-                          showDialog(context: context, builder: (BuildContext context) {
-                            List<DropdownMenuItem<String>> branches = List.generate(3, (index) => DropdownMenuItem(
-                                value: "Branch $index",
-                                child: Text("Branch $index")));
-                              return DownloadDialog(branches: branches);
-                          });
-                        },
-                        child: const Text("Download")
-                    )
-                  ],
-          );
-        });
-  }
-
-  Drawer getDrawer() {
-    return Drawer(
-      elevation: 16.0,
-      child: Container(
-        child: Text("Test"),
-      ),
+      onInit: (store) {},
+      converter: (store) {
+        return store.state;
+      },
+      builder: (context, vm) {
+        return Stack(
+          children: [
+            Wrap(clipBehavior: Clip.hardEdge, children: [
+              ExpandableDataCard(
+                title: const Text("Version"),
+                buttonClick: () {
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return VersionDialog(
+                        valueUpdate: (value) => {},
+                      );
+                    },
+                  );
+                },
+                widgets: List<Widget>.generate(
+                  3,
+                  (index) {
+                    return ListTile(
+                      title: Text("a"),
+                    );
+                  },
+                ),
+              ),
+            ])
+          ],
+        );
+      },
     );
   }
 }
