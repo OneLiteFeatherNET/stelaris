ba37ea5840e01678f743c39e283001f2e76d285c
theEvilReaper <steffenwx@gmail.com>
2023-01-20 22:59:20 +0100

    Add new SizedBox as constant

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -60,5 +60,6 @@ Icon saveIcon = const Icon(Icons.save);
 // Boxes
 const SizedBox spaceFiveBox = SizedBox(width: 5);
 const SizedBox spaceTenAndTenBox = SizedBox(width: 10, height: 10);
-const SizedBox spaceTenBox = SizedBox(width: 10,);
+const SizedBox spaceTenBox = SizedBox(width: 10);
+const SizedBox fifteenBox = SizedBox(height: 15);
 const SizedBox spaceTwentyFiveHeightBox = SizedBox(height: 25);
