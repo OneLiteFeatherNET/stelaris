aae93e84211d567d6be04575792a4216a4195068
Phillipp Glanz <p.glanz@madfix.me>
2023-01-26 21:39:53 +0100

    Fix delete method

--- lib/api/service/block_api.dart
+++ lib/api/service/block_api.dart
@@ -46,7 +46,7 @@ class BlockAPI {
   Future<BlockModel> remove(BlockModel model) async {
     final queryParams = <String, dynamic>{};
     final baseUri = Uri.parse(_apiClient.baseUrl);
-    final uri = baseUri.replace(queryParameters: queryParams, path: '${baseUri.path}/block/delete/${model.id}');
+    final uri = baseUri.replace(queryParameters: queryParams, path: '${baseUri.path}/block/remove/${model.id}');
     final result = await _apiClient.dio.deleteUri(uri).then((value) => BlockModel.fromJson(value.data!));
     return result;
   }
