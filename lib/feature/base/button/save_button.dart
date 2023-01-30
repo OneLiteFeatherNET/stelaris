58fa05fce5e1d3310742ec3e9b563d64491e96ab
theEvilReaper <steffenwx@gmail.com>
2023-01-30 20:18:38 +0100

    Improve text handling

--- lib/feature/base/button/save_button.dart
+++ lib/feature/base/button/save_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:stelaris_ui/util/I10n_ext.dart';
 
 import '../../../util/constants.dart';
 
@@ -18,7 +19,7 @@ class SaveButton extends StatelessWidget {
           onPressed: () {
             callback.call();
           },
-          label: saveText,
+          label: Text(context.l10n.button_add),
           icon: saveIcon,
         )
     );
