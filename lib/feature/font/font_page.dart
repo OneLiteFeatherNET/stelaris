ed3c50de93034c5b971f255a47d63b1e61906bbc
theEvilReaper <steffenwx@gmail.com>
2023-02-02 23:00:20 +0100

    Out source code in own widgets

--- lib/feature/font/font_page.dart
+++ lib/feature/font/font_page.dart
@@ -1,27 +1,19 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:nil/nil.dart';
-import 'package:stelaris_ui/api/api_service.dart';
 import 'package:stelaris_ui/api/model/font_model.dart';
 import 'package:stelaris_ui/api/state/actions/font_actions.dart';
 import 'package:stelaris_ui/api/util/minecraft/font_type.dart';
 import 'package:stelaris_ui/feature/base/base_layout.dart';
-import 'package:stelaris_ui/feature/base/button/save_button.dart';
-import 'package:stelaris_ui/feature/base/cards/dropdown_card.dart';
-import 'package:stelaris_ui/feature/base/cards/text_input_card.dart';
 import 'package:stelaris_ui/feature/base/model_container_list.dart';
-import 'package:stelaris_ui/feature/dialogs/delete_dialog.dart';
-import 'package:stelaris_ui/feature/dialogs/item_flag_dialog.dart';
 import 'package:stelaris_ui/feature/dialogs/stepper/setup_stepper.dart';
+import 'package:stelaris_ui/feature/font/font_general_page.dart';
+import 'package:stelaris_ui/feature/font/font_meta_page.dart';
 import 'package:stelaris_ui/util/I10n_ext.dart';
 import 'package:stelaris_ui/util/constants.dart';
 
 import '../../api/state/app_state.dart';
 import '../../api/tabs/tab_pages.dart';
-import '../base/cards/expandable_data_card.dart';
-
-const List<FontType> values = FontType.values;
 
 class FontPage extends StatefulWidget {
   const FontPage({Key? key}) : super(key: key);
@@ -110,227 +102,20 @@ class FontPageState extends State<FontPage> with BaseLayout {
     switch (e) {
       case TabPages.general:
         return ValueListenableBuilder<FontModel?>(
-            valueListenable: test,
-            builder: (BuildContext context, FontModel? value, Widget? child) {
-              return getGeneralContent(value);
-            });
+          valueListenable: test,
+          builder: (BuildContext context, FontModel? value, Widget? child) {
+            if (value == null) return nil;
+            return FontGeneralPage(model: value, selectedItem: selectedItem);
+          },
+        );
       case TabPages.meta:
         return ValueListenableBuilder<FontModel?>(
-            valueListenable: test,
-            builder: (BuildContext context, FontModel? value, Widget? child) {
-              return getMetaContent(value);
-            });
-    }
-  }
-
-  Widget getGeneralContent(FontModel? model) {
-    if (model == null) {
-      return nil;
-    }
-    return Stack(
-      children: [
-        Wrap(
-          children: [
-            TextInputCard<String>(
-              infoText: context.l10n.tooltip_name,
-              title: Text(context.l10n.card_name),
-              currentValue: model.name ?? empty,
-              formatter: [FilteringTextInputFormatter.allow(stringPattern)],
-              valueUpdate: (value) {
-                if (value == model.name) return;
-                final oldModel = model;
-                final newEntry = oldModel.copyWith(name: value);
-                setState(() {
-                  StoreProvider.dispatch(
-                      context, UpdateFontAction(oldModel, newEntry));
-                  selectedItem.value = newEntry;
-                });
-              },
-            ),
-            TextInputCard<String>(
-              infoText: context.l10n.tooltip_description,
-              title: Text(context.l10n.card_description),
-              currentValue: model.description ?? empty,
-              formatter: [FilteringTextInputFormatter.allow(stringPattern)],
-              valueUpdate: (value) {
-                if (value == model.description) return;
-                final oldModel = model;
-                final newEntry = oldModel.copyWith(description: value);
-                setState(() {
-                  StoreProvider.dispatch(
-                      context, UpdateFontAction(oldModel, newEntry));
-                  selectedItem.value = newEntry;
-                });
-              },
-            ),
-            DropDownCard<FontType, FontModel>(
-              title: Text(context.l10n.card_type),
-              items: getItems(),
-              currentValue: model,
-              defaultValue: getDefaultValue,
-              valueUpdate: (FontType? value) {
-                if (value == getDefaultValue(model)) return;
-                final newEntry = model.copyWith(type: value?.displayName);
-                setState(() {
-                  StoreProvider.dispatch(
-                      context, UpdateFontAction(model, newEntry));
-                  selectedItem.value = newEntry;
-                });
-              },
-            ),
-            TextInputCard<String>(
-              infoText: context.l10n.tooltip_ascent,
-              title: Text(context.l10n.card_ascent),
-              currentValue: model.ascent?.toString() ?? zero,
-              valueUpdate: (value) {
-                if (value == model.ascent) return;
-                final oldModel = model;
-                final newAscent = int.parse(value);
-                final newEntry = oldModel.copyWith(ascent: newAscent);
-                setState(() {
-                  StoreProvider.dispatch(
-                      context, UpdateFontAction(oldModel, newEntry));
-                  selectedItem.value = newEntry;
-                });
-              },
-              inputType: numberInput,
-              formatter: [FilteringTextInputFormatter.allow(numberPattern)],
-            ),
-            TextInputCard(
-              infoText: context.l10n.tooltip_height,
-              title: Text(context.l10n.card_height),
-              currentValue: model.height?.toString() ?? zero,
-              valueUpdate: (value) {
-                if (value == model.height) return;
-                final oldModel = model;
-                final newHeight = int.parse(value);
-                final newEntry = oldModel.copyWith(height: newHeight);
-                setState(() {
-                  StoreProvider.dispatch(
-                      context, UpdateFontAction(oldModel, newEntry));
-                  selectedItem.value = newEntry;
-                });
-              },
-              inputType: numberInput,
-              formatter: [FilteringTextInputFormatter.allow(numberPattern)],
-            ),
-          ],
-        ),
-        SaveButton(
-          callback: () {
-            ApiService().fontAPI.update(model);
+          valueListenable: test,
+          builder: (BuildContext context, FontModel? value, Widget? child) {
+            if (value == null) return nil;
+            return FontMetaPage(model: value, selectedItem: selectedItem);
           },
-        )
-      ],
-    );
-  }
-
-  Widget getMetaContent(FontModel? model) {
-    if (model == null) return nil;
-
-    return Stack(
-      children: [
-        Wrap(
-          clipBehavior: Clip.hardEdge,
-          children: [
-            ExpandableDataCard(
-              title: const Text("Chars"),
-              buttonClick: () {
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return EntryAddDialog(
-                        title: const Text("Add new char"),
-                        controller: TextEditingController(),
-                        formatters: [
-                          FilteringTextInputFormatter.allow(stringPattern)
-                        ],
-                        valueUpdate: (value) {
-                          final oldEntry = model;
-                          List<String> chars = List.of(oldEntry.chars ?? []);
-                          chars.add(value);
-                          final newEntry = oldEntry.copyWith(chars: chars);
-                          setState(() {
-                            StoreProvider.dispatch(
-                                context, UpdateFontAction(oldEntry, newEntry));
-                            Navigator.pop(context);
-                            selectedItem.value = newEntry;
-                          });
-                        });
-                  },
-                );
-              },
-              widgets: List<Widget>.generate(
-                model.chars?.length ?? 0,
-                (index) {
-                  final key = model.chars![index];
-                  return ListTile(
-                    title: Text(key),
-                    trailing: IconButton(
-                      icon: deleteIcon,
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (context) {
-                            return DeleteDialog<String>(
-                                title: Text(context.l10n.dialog_delete_confirm),
-                                header: [
-                                  TextSpan(
-                                      text:
-                                      context.l10n.delete_dialog_first_line,
-                                      style: TextStyle(color: Colors.white)),
-                                  TextSpan(
-                                      text: key,
-                                      style:
-                                      const TextStyle(color: Colors.red)),
-                                  TextSpan(
-                                      text: context.l10n.delete_dialog_entry,
-                                      style: TextStyle(color: Colors.white)),
-                                ],
-                                value: key,
-                                successfully: (value) {
-                                  final oldEntry = model;
-                                  List<String> chars = List.of(model.chars ?? []);
-                                  chars.remove(key);
-                                  final newEntry = oldEntry.copyWith(chars: chars);
-                                  setState(() {
-                                    StoreProvider.dispatch(context,
-                                        UpdateFontAction(oldEntry, newEntry));
-                                    selectedItem.value = newEntry;
-                                  });
-                                  return true;
-                                }
-                            );
-                          },
-                        );
-                      },
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-        SaveButton(
-          callback: () {
-            ApiService().fontAPI.update(model);
-          },
-        )
-      ],
-    );
-  }
-
-  List<DropdownMenuItem<FontType>>? getItems() {
-    return List.generate(
-        values.length,
-        (index) => DropdownMenuItem(
-              value: values[index],
-              child: Text(values[index].displayName),
-            ));
-  }
-
-  FontType getDefaultValue(FontModel value) {
-    return FontType.values
-        .firstWhere((element) => element.displayName == value.type);
+        );
+    }
   }
 }
