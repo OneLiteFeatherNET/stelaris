ddea4985318ee6a7de57a7b66ce6b1c96b653f03
theEvilReaper <steffenwx@gmail.com>
2023-02-12 19:55:13 +0100

    Add own widget class for the button to delete an entry

---
+++ lib/feature/base/button/delete_entry_button.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:stelaris_ui/feature/dialogs/delete_dialog.dart';
+import 'package:stelaris_ui/util/constants.dart';
+import 'package:stelaris_ui/util/typedefs.dart';
+
+class DeleteEntryButton<E> extends StatelessWidget {
+  final String title;
+  final List<TextSpan> header;
+  final E value;
+  final MapToDeleteSuccessfully<E> mapToDeleteSuccessfully;
+
+  const DeleteEntryButton(
+      {Key? key,
+      required this.title,
+      required this.header,
+      required this.value,
+      required this.mapToDeleteSuccessfully})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      icon: deleteIcon,
+      onPressed: () {
+        showDialog(
+            context: context,
+            builder: (context) {
+              return DeleteDialog<E>(
+                  title: Text(
+                    title,
+                    textAlign: TextAlign.center,
+                  ),
+                  header: header,
+                  value: value,
+                  successfully: mapToDeleteSuccessfully);
+            });
+      },
+    );
+  }
+}
