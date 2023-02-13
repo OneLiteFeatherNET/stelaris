fff879f78277d465b942736f1d1b53f72e1d28b9
Joltra <MinnyMouseXDmail.com>
2023-02-13 22:32:04 +0100

    Wrap widgets into a form and add key

--- lib/feature/font/font_general_page.dart
+++ lib/feature/font/font_general_page.dart
@@ -13,95 +13,114 @@ import 'package:stelaris_ui/util/constants.dart';
 const List<FontType> values = FontType.values;
 
 class FontGeneralPage extends StatefulWidget {
-
   final FontModel model;
   final ValueNotifier<FontModel?> selectedItem;
 
-  const FontGeneralPage({Key? key, required this.model, required this.selectedItem}) : super(key: key);
+  const FontGeneralPage(
+      {Key? key, required this.model, required this.selectedItem})
+      : super(key: key);
 
   @override
   State<FontGeneralPage> createState() => _FontGeneralPageState();
 }
 
 class _FontGeneralPageState extends State<FontGeneralPage> {
+
+  final _key = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Wrap(
-          children: [
-            TextInputCard<String>(
-              infoText: context.l10n.tooltip_name,
-              title: Text(context.l10n.card_name),
-              currentValue: widget.model.name ?? empty,
-              formatter: [FilteringTextInputFormatter.allow(stringPattern)],
-              valueUpdate: (value) {
-                if (value == widget.model.name) return;
-                final oldModel = widget.model;
-                final newEntry = oldModel.copyWith(name: value);
-                setState(() {
-                  StoreProvider.dispatch(
-                      context, UpdateFontAction(oldModel, newEntry));
-                  widget.selectedItem.value = newEntry;
-                });
-              },
-            ),
-            TextInputCard<String>(
-              infoText: context.l10n.tooltip_description,
-              title: Text(context.l10n.card_description),
-              currentValue: widget.model.description ?? empty,
-              formatter: [FilteringTextInputFormatter.allow(stringPattern)],
-              valueUpdate: (value) {
-                if (value == widget.model.description) return;
-                final oldModel = widget.model;
-                final newEntry = oldModel.copyWith(description: value);
-                setState(() {
-                  StoreProvider.dispatch(
-                      context, UpdateFontAction(oldModel, newEntry));
-                  widget.selectedItem.value = newEntry;
-                });
-              },
-            ),
-            TextInputCard<String>(
-              infoText: context.l10n.tooltip_ascent,
-              title: Text(context.l10n.card_ascent),
-              currentValue: widget.model.ascent?.toString() ?? zero,
-              valueUpdate: (value) {
-                if (value == widget.model.ascent) return;
-                final oldModel = widget.model;
-                final newAscent = int.parse(value);
-                final newEntry = oldModel.copyWith(ascent: newAscent);
-                setState(() {
-                  StoreProvider.dispatch(
-                      context, UpdateFontAction(oldModel, newEntry));
-                  widget.selectedItem.value = newEntry;
-                });
-              },
-              inputType: numberInput,
-              formatter: [FilteringTextInputFormatter.allow(numberPattern)],
-            ),
-            TextInputCard(
-              infoText: context.l10n.tooltip_height,
-              title: Text(context.l10n.card_height),
-              currentValue: widget.model.height?.toString() ?? zero,
-              valueUpdate: (value) {
-                if (value == widget.model.height) return;
-                final oldModel = widget.model;
-                final newHeight = int.parse(value);
-                final newEntry = oldModel.copyWith(height: newHeight);
-                setState(() {
-                  StoreProvider.dispatch(
-                      context, UpdateFontAction(oldModel, newEntry));
-                  widget.selectedItem.value = newEntry;
-                });
-              },
-              inputType: numberInput,
-              formatter: [FilteringTextInputFormatter.allow(numberPattern)],
-            ),
-          ],
+        Form(
+          autovalidateMode: AutovalidateMode.always,
+          key: _key,
+          child: Wrap(
+            children: [
+              TextInputCard<String>(
+                infoText: context.l10n.tooltip_name,
+                title: Text(context.l10n.card_name),
+                currentValue: widget.model.name ?? empty,
+                formatter: [FilteringTextInputFormatter.allow(stringPattern)],
+                valueUpdate: (value) {
+                  if (value == widget.model.name) return;
+                  final oldModel = widget.model;
+                  final newEntry = oldModel.copyWith(name: value);
+                  setState(() {
+                    StoreProvider.dispatch(
+                        context, UpdateFontAction(oldModel, newEntry));
+                    widget.selectedItem.value = newEntry;
+                  });
+                },
+              ),
+              TextInputCard<String>(
+                infoText: context.l10n.tooltip_description,
+                title: Text(context.l10n.card_description),
+                currentValue: widget.model.description ?? empty,
+                formatter: [FilteringTextInputFormatter.allow(stringPattern)],
+                valueUpdate: (value) {
+                  if (value == widget.model.description) return;
+                  final oldModel = widget.model;
+                  final newEntry = oldModel.copyWith(description: value);
+                  setState(() {
+                    StoreProvider.dispatch(
+                        context, UpdateFontAction(oldModel, newEntry));
+                    widget.selectedItem.value = newEntry;
+                  });
+                },
+              ),
+              TextInputCard<String>(
+                infoText: context.l10n.tooltip_ascent,
+                title: Text(context.l10n.card_ascent),
+                currentValue: widget.model.ascent?.toString() ?? zero,
+                valueUpdate: (value) {
+                  if (value == widget.model.ascent) return;
+                  final oldModel = widget.model;
+                  final newAscent = int.parse(value);
+                  final newEntry = oldModel.copyWith(ascent: newAscent);
+                  setState(() {
+                    StoreProvider.dispatch(
+                        context, UpdateFontAction(oldModel, newEntry));
+                    widget.selectedItem.value = newEntry;
+                  });
+                },
+                inputType: numberInput,
+                formatter: [FilteringTextInputFormatter.allow(numberPattern)],
+              ),
+              TextInputCard(
+                infoText: context.l10n.tooltip_height,
+                title: Text(context.l10n.card_height),
+                currentValue: widget.model.height?.toString() ?? zero,
+                valueUpdate: (value) {
+                  if (value == widget.model.height) return;
+                  final oldModel = widget.model;
+                  final newHeight = int.tryParse(value);
+                  final newEntry = oldModel.copyWith(height: newHeight);
+                  setState(() {
+                    StoreProvider.dispatch(
+                        context, UpdateFontAction(oldModel, newEntry));
+                    widget.selectedItem.value = newEntry;
+                  });
+                },
+                inputType: numberInput,
+                formatter: [FilteringTextInputFormatter.allow(numberPattern)],
+                validator: (p0) {
+                  if (p0 == null || int.tryParse(p0) == null) {
+                    return true;
+                  }
+                  if (int.tryParse(p0)! > 10) {
+                    return true;
+                  }
+                  return false;
+                },
+                errorMessage: "Value is to high",
+              ),
+            ],
+          ),
         ),
         SaveButton(
           callback: () {
+            if (!_key.currentState!.validate()) return;
             ApiService().fontAPI.update(widget.model);
           },
         )
@@ -112,10 +131,10 @@ class _FontGeneralPageState extends State<FontGeneralPage> {
   List<DropdownMenuItem<FontType>>? getItems() {
     return List.generate(
         values.length,
-            (index) => DropdownMenuItem(
-          value: values[index],
-          child: Text(values[index].displayName),
-        ));
+        (index) => DropdownMenuItem(
+              value: values[index],
+              child: Text(values[index].displayName),
+            ));
   }
 
   FontType getDefaultValue(FontModel value) {
