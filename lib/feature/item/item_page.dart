38ae6588a1c34078bd8747df756220dc44892a87
theEvilReaper <steffenwx@gmail.com>
2023-03-11 13:14:38 +0100

    Remove unused import

--- lib/feature/item/item_page.dart
+++ lib/feature/item/item_page.dart
@@ -1,6 +1,5 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import 'package:nil/nil.dart';
 import 'package:stelaris_ui/api/model/item_model.dart';
 import 'package:stelaris_ui/api/state/actions/item_actions.dart';
