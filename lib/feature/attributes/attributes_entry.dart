9f9e07924e7a0522470785e1e2672f9dc8af786f
Joltra <joltra@onelitefeather.net>
2023-10-26 10:26:22 +0200

    Use the attribute widget, use the model values as initial values for the text editing controllers

--- lib/feature/attributes/attributes_entry.dart
+++ lib/feature/attributes/attributes_entry.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:stelaris_ui/api/model/attribute_model.dart';
+import 'package:stelaris_ui/feature/attributes/attribute_row.dart';
 import 'package:stelaris_ui/feature/attributes/attributes_tile.dart';
 import 'package:stelaris_ui/feature/base/base_layout.dart';
-import 'package:stelaris_ui/feature/base/button/delete_entry_button.dart';
 
 class AttributesEntry extends StatelessWidget with BaseLayout {
 
@@ -12,66 +12,24 @@ class AttributesEntry extends StatelessWidget with BaseLayout {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    var nameController = TextEditingController(text: attributeModel.name ?? "");
+    var defaultController = TextEditingController(text: "${attributeModel.defaultValue}");
+    var maxValueController = TextEditingController(text: "${attributeModel.maximumValue}");
+    return Container(
+      width: 750,
       padding: padding,
       child: constructContainer(
         [
           ExpansionTile(
               controlAffinity: ListTileControlAffinity.trailing,
               title: AttributesTile(attributeModel: attributeModel,),
-              children: [
+
+              children:
+              [
+                AttributeRow(controller: nameController, label: "Name"),
+                AttributeRow(controller: defaultController, label: "Default"),
+                AttributeRow(controller: maxValueController, label: "Maximum"),
                 SizedBox(height: 10,),
-                Row(
-                  children: [
-                    // Add your Row children here
-                    Text("Test"),
-                    SizedBox(width: 10,),
-                    Expanded(
-                      child: FractionallySizedBox(
-                        widthFactor: 0.9,
-                        child: TextFormField(
-                          controller: TextEditingController(),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(height: 20,),
-                Row(
-                  children: [
-                    // Add your Row children here
-                    Text("Test"),
-                    SizedBox(width: 10,),
-                    Expanded(
-                      child: FractionallySizedBox(
-                        widthFactor: 0.9,
-                        child: TextFormField(
-                          controller: TextEditingController(),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-                SizedBox(height: 20,),
-                Padding(
-                  padding: padding,
-                  child: Row(
-                    children: [
-                      // Add your Row children here
-                      Text("Test"),
-                      SizedBox(width: 10,),
-                      Expanded(
-                        child: FractionallySizedBox(
-                          widthFactor: 0.9,
-                          child: TextFormField(
-                            controller: TextEditingController(),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(height: 20,)
               ],
           )
         ],
