54b657fc45d7d3a6183a5feb5b6101e57db566e6
Joltra <joltra@onelitefeather.net>
2023-10-26 10:28:30 +0200

    Add setup dialog, get attribute models from the store

--- lib/feature/attributes/attributes_page.dart
+++ lib/feature/attributes/attributes_page.dart
@@ -1,5 +1,9 @@
+import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
+import 'package:stelaris_ui/api/state/actions/attribute_actions.dart';
+import 'package:stelaris_ui/api/state/app_state.dart';
 import 'package:stelaris_ui/feature/attributes/attributes_entry.dart';
+import 'package:stelaris_ui/feature/dialogs/setup_dialog.dart';
 
 import '../../api/model/attribute_model.dart';
 import '../base/position_bottom_right.dart';
@@ -9,26 +13,51 @@ class AttributesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    AttributeAddAction(AttributeModel(modelName: "test"));
     return Stack(
       children: [
-        SingleChildScrollView(
-
-          child: Wrap(
-            clipBehavior: Clip.hardEdge,
-            children: [
-              const SizedBox(height: 10),
-              const Text("Attributes"),
-              const SizedBox(height: 10),
-              const AttributesEntry(attributeModel: AttributeModel(name: "Test", defaultValue: 1, maximumValue: 10,)),
-            ],
+        SizedBox(
+          width: double.infinity,
+          child: SingleChildScrollView(
+            child: StoreConnector<AppState, List<AttributeModel>>(
+                onInit: (store) {
+                  store.dispatch(InitAttributeListAction());
+                },
+                converter: (store) => store.state.attributes,
+                builder: (BuildContext context, vm) {
+                  return Wrap(
+                    clipBehavior: Clip.hardEdge,
+                    children: vm
+                        .map((e) => AttributesEntry(
+                              attributeModel: e,
+                            ))
+                        .toList(),
+                  );
+                }),
           ),
         ),
         PositionBottomRight(
           child: FloatingActionButton.extended(
             heroTag: UniqueKey(),
-            onPressed: () {},
-            label: Text("Add"),
-            icon: Icon(Icons.add),
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return SetUpDialog<AttributeModel>(
+                    buildModel: (name, description) {
+                      return AttributeModel(
+                          modelName: name);
+                    },
+                    finishCallback: (model) {
+                      StoreProvider.dispatch(context, AttributeAddAction(model));
+                      Navigator.pop(context);
+                    },
+                  );
+                },
+              );
+            },
+            label: const Text("Add"),
+            icon: const Icon(Icons.add),
           ),
         ),
       ],
