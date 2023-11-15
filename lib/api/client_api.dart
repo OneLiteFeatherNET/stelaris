5872d4701611aaaccc3548290a0c04845a2ae1df
Joltra <joltra@onelitefeather.net>
2023-11-15 21:14:58 +0100

    Add client api definition which contains the most common functions to interact with a backend

---
+++ lib/api/client_api.dart
@@ -0,0 +1,15 @@
+import 'package:stelaris_ui/api/model/data_model.dart';
+
+/// The class represents each function which a implementation fo the ClientAPI should have.
+abstract class ClientAPI<T extends DataModel> {
+
+  Future<T> get();
+
+  Future<T> add(T model);
+
+  Future<List<T>> getAll();
+
+  Future<T> update(T model);
+
+  Future<T> remove(T model);
+}
