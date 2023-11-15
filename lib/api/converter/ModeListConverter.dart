8fe8017bb941a057d963d5af30c7a948dc8e8edc
Joltra <joltra@onelitefeather.net>
2023-11-15 21:14:24 +0100

    Add generic implementation of the model list conversation

---
+++ lib/api/converter/ModeListConverter.dart
@@ -0,0 +1,18 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:stelaris_ui/api/model/data_model.dart';
+
+class ModelListConverter<T extends DataModel> implements JsonConverter<List<T>, List<dynamic>> {
+
+  final T Function(dynamic) fromJsonConverter;
+
+  const ModelListConverter(this.fromJsonConverter);
+
+  @override
+  List<T> fromJson(List<dynamic> json) {
+    return json.map((e) => fromJsonConverter(e)).toList();
+  }
+  @override
+  List<dynamic> toJson(List<T> object) {
+    throw UnimplementedError();
+  }
+}
