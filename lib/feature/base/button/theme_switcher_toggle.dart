ea2f75c7728cc6c2eb954a58e64371a2b1cf42fb
Joltras <MinnyMouseXD@gmail.com>
2024-01-22 19:20:58 +0100

    Change theme switcher to a stateless widget

--- lib/feature/base/button/theme_switcher_toggle.dart
+++ lib/feature/base/button/theme_switcher_toggle.dart
@@ -4,27 +4,23 @@ import 'package:stelaris_ui/feature/home/home.dart';
 const Icon nightMode = Icon(Icons.nights_stay_sharp);
 const Icon lightMode = Icon(Icons.sunny_snowing);
 
-class ThemeSwitcherToggle extends StatefulWidget {
+class ThemeSwitcherToggle extends StatelessWidget {
 
-  const ThemeSwitcherToggle({super.key});
-
-  @override
-  State<ThemeSwitcherToggle> createState() => _ThemeSwitcherToggleState();
-}
-
-class _ThemeSwitcherToggleState extends State<ThemeSwitcherToggle> {
+  const ThemeSwitcherToggle({
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return IconButton(
         onPressed: () {
-          setState(() => _changeNightModeState());
+          _changeNightModeState();
         },
-        icon: notifier.value == ThemeMode.light ? lightMode : nightMode
-    );
+        icon: notifier.value == ThemeMode.light ? lightMode : nightMode);
   }
 
   void _changeNightModeState() {
-    notifier.value = notifier.value == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    notifier.value =
+        notifier.value == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
   }
 }
