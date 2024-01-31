639271d072a4078b9e94566bd07a283964f4d621
Joltras <MinnyMouseXD@gmail.com>
2024-01-31 10:14:28 +0100

    Add widget for the header of the expandable data card v2

---
+++ lib/feature/base/cards/expandable_header.dart
@@ -0,0 +1,40 @@
+import 'package:expandable/expandable.dart';
+import 'package:flutter/material.dart';
+
+import '../../../util/constants.dart';
+
+class ExpandableHeader extends StatelessWidget {
+  final bool isExpanded;
+  final Text title;
+  final VoidCallback buttonClick;
+  const ExpandableHeader({
+    super.key,
+    required this.isExpanded,
+    required this.title,
+    required this.buttonClick,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpandableButton(
+      child: Container(
+        decoration: BoxDecoration(
+          color: Theme.of(context).secondaryHeaderColor,
+          borderRadius: BorderRadius.circular(5),
+        ),
+        child: ListTile(
+          title: title,
+          leading: Icon(
+            isExpanded ? Icons.expand_less : Icons.expand_more,
+            color: isExpanded ? Colors.tealAccent : Colors.white,
+          ),
+          trailing: IconButton(
+            onPressed: buttonClick,
+            icon: addModelIcon,
+            color: isExpanded ? Colors.tealAccent : Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
