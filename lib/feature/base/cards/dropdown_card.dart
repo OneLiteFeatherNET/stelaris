4e0f73cf8148c9a8dc7ccc59822e30e79fe1d0eb
Joltras <MinnyMouseXD@gmail.com>
2024-02-05 19:02:17 +0100

    Update dropdown card widget

--- lib/feature/base/cards/dropdown_card.dart
+++ lib/feature/base/cards/dropdown_card.dart
@@ -1,39 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:stelaris_ui/feature/base/base_card.dart';
 import 'package:stelaris_ui/feature/base/base_layout.dart';
 import 'package:stelaris_ui/util/typedefs.dart';
 
-class DropDownCard<E, T> extends StatefulWidget {
-  final Text title;
-  final List<DropdownMenuItem<E>>? items;
+class DropdownCard<E, T> extends StatefulWidget {
+  final String display;
   final ValueUpdate<E> valueUpdate;
   final DefaultValue<E, T> defaultValue;
   final T currentValue;
+  final String? tooltipMessage;
+  final List<DropdownMenuItem<E>>? items;
   final Key? formKey;
 
-  const DropDownCard(
-      {super.key,
-      required this.title,
-      required this.items,
-      required this.valueUpdate,
-      required this.defaultValue,
-      required this.currentValue,
-      this.formKey});
+  const DropdownCard({
+    super.key,
+    required this.display,
+    required this.valueUpdate,
+    required this.defaultValue,
+    required this.currentValue,
+    this.tooltipMessage,
+    this.items,
+    this.formKey,
+  });
 
   @override
-  State<DropDownCard<E, T>> createState() => _DropDownCardState<E, T>();
+  State<DropdownCard<E, T>> createState() => _DropdownCardState<E, T>();
 }
 
-class _DropDownCardState<E, T> extends State<DropDownCard<E, T>>
-    with BaseLayout {
+class _DropdownCardState<E, T> extends State<DropdownCard<E, T>> {
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: padding,
-      child: constructContainer([
-        widget.title,
-        spaceBox,
-        SizedBox(
-            width: 300,
+      child: BaseCard(
+        display: widget.display,
+        message: widget.tooltipMessage,
+        widget: Padding(
+          padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
             child: Form(
               key: widget.formKey,
               child: DropdownButtonFormField<E>(
@@ -46,8 +51,10 @@ class _DropDownCardState<E, T> extends State<DropDownCard<E, T>>
                   });
                 },
               ),
-            ))
-      ]),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
