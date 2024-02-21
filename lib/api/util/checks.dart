4ddc09ea60edfc1522f8a9de7dadc0e5cb69a09d
theEvilReaper <steffenwx@gmail.com>
2024-02-21 15:28:24 +0100

    Remove deprecated code

--- lib/api/util/checks.dart
+++
@@ -1,9 +0,0 @@
-@Deprecated('Will be deleted in the next time')
-class Checks {
-
-  static void argCondition(bool check, String message) {
-    if (check) {
-      throw FormatException(message);
-    }
-  }
-}
