70bb4f92fdf762e0f26d44777eace406cabc57ff
theEvilReaper <steffenwx@gmail.com>
2024-05-06 16:39:52 +0200

    Remove block integration

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,6 +1,5 @@
 import 'package:stelaris_ui/api/api_client.dart';
 import 'package:stelaris_ui/api/service/attributes_api.dart';
-import 'package:stelaris_ui/api/service/block_api.dart';
 import 'package:stelaris_ui/api/service/download_api.dart';
 import 'package:stelaris_ui/api/service/font_api.dart';
 import 'package:stelaris_ui/api/service/generate_api.dart';
@@ -28,8 +27,6 @@ class ApiService {
   late final DownloadAPI downloadAPI = DownloadAPI(_apiClient);
 
   late final FontAPI fontAPI = FontAPI(_apiClient);
-
-  late final BlockAPI blockAPI = BlockAPI(_apiClient);
 
   late final AttributesAPI attributesAPI = AttributesAPI(_apiClient);
 
--- lib/api/state/app_state.dart
+++ lib/api/state/app_state.dart
@@ -1,6 +1,5 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:stelaris_ui/api/model/attribute_model.dart';
-import 'package:stelaris_ui/api/model/block_model.dart';
 import 'package:stelaris_ui/api/model/font_model.dart';
 import 'package:stelaris_ui/api/model/item_model.dart';
 import 'package:stelaris_ui/api/model/notification_model.dart';
@@ -15,7 +14,6 @@ class AppState with _$AppState {
     @Default([]) List<ItemModel> items,
     @Default([]) List<NotificationModel> notifications,
     @Default([]) List<FontModel> fonts,
-    @Default([]) List<BlockModel> blocks,
     @Default(<AttributeModel>[]) List<AttributeModel> attributes,
     @Default(true) bool openNavigation,
     @Default(true) bool nightMode,
@@ -23,7 +21,6 @@ class AppState with _$AppState {
     @JsonKey(includeToJson: false)@Default(null) NotificationModel? selectedNotification,
     @JsonKey(includeToJson: false)@Default(null) FontModel? selectedFont,
     @JsonKey(includeToJson: false)@Default(null) AttributeModel? selectedAttribute,
-    @JsonKey(includeToJson: false)@Default(null) BlockModel? selectedBlock,
   }) = _AppState;
 
   factory AppState.fromJson(Map<String, dynamic> json) =>
