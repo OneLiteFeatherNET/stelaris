f226d64220b30605a28d0a4e05f1d9c8df5ed9cd
Steffen <theevilreaper@onelitefeather.net>
2024-05-12 14:13:19 +0000

    Add environment file

---
+++ lib/env/environment.dart
@@ -0,0 +1,6 @@
+/// This file contains the environment variables for the app
+/// The pipeline should replace this constant with the right data
+class Environment {
+  static const String backendURl = '';
+  static const String generatorUrl = '';
+}
