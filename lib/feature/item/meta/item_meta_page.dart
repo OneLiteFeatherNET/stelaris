b4e8a3ec7987c25b29f7a14361f4092e9488e2f1
Steffen <theevilreaper@onelitefeather.net>
2024-05-14 20:43:53 +0000

    Revise flag selection

--- lib/feature/item/meta/item_meta_page.dart
+++ lib/feature/item/meta/item_meta_page.dart
@@ -1,44 +1,68 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
-import 'package:stelaris_ui/api/state/actions/item_actions.dart';
 import 'package:stelaris_ui/api/state/app_state.dart';
 import 'package:stelaris_ui/api/state/factory/item/selected_item_state.dart';
-import 'package:stelaris_ui/feature/base/button/save_button.dart';
-import 'package:stelaris_ui/feature/item/meta/enchantment_card.dart';
-import 'package:stelaris_ui/feature/item/meta/flag_card.dart';
 import 'package:stelaris_ui/feature/item/item_reducer.dart';
+import 'package:stelaris_ui/feature/item/meta/enchantment_card.dart';
+import 'package:stelaris_ui/feature/item/meta/flag_grid.dart';
 import 'package:stelaris_ui/util/constants.dart';
 
-class ItemMetaPage extends StatelessWidget with DropDownItemReducer {
+class ItemMetaPage extends StatefulWidget with DropDownItemReducer {
 
   ItemMetaPage({
     super.key,
   });
 
   @override
+  State<ItemMetaPage> createState() => _ItemMetaPageState();
+}
+
+class _ItemMetaPageState extends State<ItemMetaPage>
+    with TickerProviderStateMixin {
+  late final TabController _tabController;
+
+  @override
+  void initState() {
+    _tabController = TabController(length: 2, vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, SelectedItemView>(
-      vm: () => SelectedItemFactory(),
-      builder: (context, vm) {
-        return Stack(
-          children: [
-            SingleChildScrollView(
-              child: Wrap(
-                clipBehavior: Clip.hardEdge,
-                children: [
-                  verticalSpacing10,
-                  FlagCard(model: vm.selected),
-                  verticalSpacing10,
-                  EnchantmentCard(model: vm.selected),
-                ],
-              ),
+        vm: () => SelectedItemFactory<ItemMetaPage>(),
+        builder: (context, vm) {
+          return  Padding(
+            padding: const EdgeInsets.only(top: 20),
+            child: Column(
+              children: [
+                TabBar.secondary(
+                  controller: _tabController,
+                  tabs: const [
+                    Tab(text: 'ItemFlags',),
+                    Tab(text: 'Enchantments',),
+                  ],
+                ),
+                heightTen,
+                Expanded(
+                  child: TabBarView(
+                    controller: _tabController,
+                    children: [
+                      const FlagGrid(),
+                      EnchantmentCard(model: vm.selected),
+                    ],
+                  ),
+                ),
+              ],
             ),
-            SaveButton(
-              callback: () => context.dispatch(ItemDatabaseUpdate()),
-            )
-          ],
-        );
-      },
+          );
+        },
     );
   }
 }
