b404e9603a05c101500c7f5847b8a1cf32743cf6
Joltras <MinnyMouseXD@gmail.com>
2024-05-18 20:04:29 +0200

    Add save button back

--- lib/feature/item/meta/enchantment_card.dart
+++ lib/feature/item/meta/enchantment_card.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:stelaris_ui/api/model/item_model.dart';
 import 'package:stelaris_ui/api/state/actions/item_actions.dart';
 import 'package:stelaris_ui/feature/base/button/entry_buttons.dart';
+import 'package:stelaris_ui/feature/base/button/save_button.dart';
 import 'package:stelaris_ui/feature/base/cards/expandable_data_card.dart';
 import 'package:stelaris_ui/feature/dialogs/abort_add_dialog.dart';
 import 'package:stelaris_ui/feature/dialogs/item_enchantments_dialog.dart';
@@ -14,6 +15,7 @@ import 'package:stelaris_ui/util/functions.dart';
 
 class EnchantmentCard extends StatelessWidget with EnchantmentReducer {
   final ItemModel model;
+
   EnchantmentCard({
     required this.model,
     super.key,
@@ -21,81 +23,88 @@ class EnchantmentCard extends StatelessWidget with EnchantmentReducer {
 
   @override
   Widget build(BuildContext context) {
-    return ExpandableDataCard(
-      title: Text(context.l10n.card_enchantments),
-      buttonClick: () {
-        showDialog(
-          context: context,
-          builder: (BuildContext context) {
-            if (model.enchantments != null && !canAdd(model)) {
-              return AbortAddDialog(
-                title: context.l10n.dialog_abort_enchantment_title,
-                content: context.l10n.dialog_abort_enchantments,
-              );
-            }
-            final oldEntry = model;
-            return ItemEnchantmentAddDialog(
-              model: model,
-              formFieldValidator: (value) {
-                final input = value as String;
+    return Stack(
+      children: [
+        ExpandableDataCard(
+          title: Text(context.l10n.card_enchantments),
+          buttonClick: () {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                if (model.enchantments != null && !canAdd(model)) {
+                  return AbortAddDialog(
+                    title: context.l10n.dialog_abort_enchantment_title,
+                    content: context.l10n.dialog_abort_enchantments,
+                  );
+                }
+                final oldEntry = model;
+                return ItemEnchantmentAddDialog(
+                  model: model,
+                  formFieldValidator: (value) {
+                    final input = value as String;
 
-                return checkIfEmptyAndReturnErrorString(input, context);
-              },
-              addEnchantmentCallback: (selected, level) {
-                final oldEnchantments =
-                    Map<String, int>.of(oldEntry.enchantments ?? {});
-                oldEnchantments[selected.minecraftValue] = level;
-                final newEntry =
-                    oldEntry.copyWith(enchantments: oldEnchantments);
-                Navigator.pop(context);
-                context.dispatch(UpdateItemAction(newEntry));
+                    return checkIfEmptyAndReturnErrorString(input, context);
+                  },
+                  addEnchantmentCallback: (selected, level) {
+                    final oldEnchantments =
+                        Map<String, int>.of(oldEntry.enchantments ?? {});
+                    oldEnchantments[selected.minecraftValue] = level;
+                    final newEntry =
+                        oldEntry.copyWith(enchantments: oldEnchantments);
+                    Navigator.pop(context);
+                    context.dispatch(UpdateItemAction(newEntry));
+                  },
+                );
               },
             );
           },
-        );
-      },
-      widgets: List<Widget>.generate(
-        model.enchantments?.length ?? 0,
-        (index) {
-          final key = model.enchantments?.keys.elementAt(index);
-          final value = model.enchantments?.values.elementAt(index);
-          return ListTile(
-            title: Text('$key, Level: $value'),
-            trailing: EntryButtons(
-              editTitle: context.l10n.dialog_level_title,
-              model: model,
-              name: key,
-              value: value.toString(),
-              inputFormatters: [
-                FilteringTextInputFormatter.allow(numberPattern)
-              ],
-              formFieldValidator: (value) {
-                final input = value as String;
+          widgets: List<Widget>.generate(
+            model.enchantments?.length ?? 0,
+            (index) {
+              final key = model.enchantments?.keys.elementAt(index);
+              final value = model.enchantments?.values.elementAt(index);
+              return ListTile(
+                title: Text('$key, Level: $value'),
+                trailing: EntryButtons(
+                  editTitle: context.l10n.dialog_level_title,
+                  model: model,
+                  name: key,
+                  value: value.toString(),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(numberPattern)
+                  ],
+                  formFieldValidator: (value) {
+                    final input = value as String;
 
-                return checkIfEmptyAndReturnErrorString(input, context);
-              },
-              delete: (ItemModel? value) {
-                final oldEntry = model;
-                final oldEnchantments =
-                    Map<String, int>.of(oldEntry.enchantments ?? {});
-                oldEnchantments.remove(key);
-                final newEntry =
-                    oldEntry.copyWith(enchantments: oldEnchantments);
-                context.dispatch(UpdateItemAction(newEntry));
-              },
-              update: (value, key) {
-                final oldEntry = model;
-                final oldEnchantments =
-                    Map<String, int>.from(oldEntry.enchantments ?? {});
-                oldEnchantments[value!] = int.parse(key!);
-                final newEntry =
-                    oldEntry.copyWith(enchantments: oldEnchantments);
-                context.dispatch(UpdateItemAction(newEntry));
-              },
-            ),
-          );
-        },
-      ),
+                    return checkIfEmptyAndReturnErrorString(input, context);
+                  },
+                  delete: (ItemModel? value) {
+                    final oldEntry = model;
+                    final oldEnchantments =
+                        Map<String, int>.of(oldEntry.enchantments ?? {});
+                    oldEnchantments.remove(key);
+                    final newEntry =
+                        oldEntry.copyWith(enchantments: oldEnchantments);
+                    context.dispatch(UpdateItemAction(newEntry));
+                  },
+                  update: (value, key) {
+                    final oldEntry = model;
+                    final oldEnchantments =
+                        Map<String, int>.from(oldEntry.enchantments ?? {});
+                    oldEnchantments[value!] = int.parse(key!);
+                    final newEntry =
+                        oldEntry.copyWith(enchantments: oldEnchantments);
+                    context.dispatch(UpdateItemAction(newEntry));
+                  },
+                ),
+              );
+            },
+          ),
+        ),
+        SaveButton(
+          callback: () => context.dispatch(ItemDatabaseUpdate()),
+        ),
+      ],
     );
   }
 }
