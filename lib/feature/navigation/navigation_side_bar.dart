63575b7c6205e65b7986ca66bc83fde238a21fe6
theEvilReaper <steffenwx@gmail.com>
2025-02-20 23:13:42 +0100

    Improve class layout, documentation and other parts

--- lib/feature/navigation/navigation_side_bar.dart
+++ lib/feature/navigation/navigation_side_bar.dart
@@ -6,41 +6,48 @@ import 'package:stelaris/api/state/factory/navigation_vm_state.dart';
 import 'package:stelaris/api/util/navigation.dart';
 
 const double maxXOffset = 180;
-const navigationEntries = NavigationEntry.values;
-const navigationEntryTextStyle = TextStyle(fontSize: 16);
+const List<NavigationEntry> navigationEntries = NavigationEntry.values;
+const TextStyle navigationEntryTextStyle = TextStyle(fontSize: 16);
 
+/// A widget that represents a navigation sidebar for the application.
+///
+/// The [NavigationSideBar] allows users to navigate between different
+/// sections of the application using a [NavigationRail].
 class NavigationSideBar extends StatelessWidget {
-
   const NavigationSideBar({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final router = GoRouterState.of(context);
-    final index = navigationEntries.indexWhere((element) {
-      return element.route == router.uri.toString();
+    final routerUri = GoRouterState.of(context).uri.toString();
+    final selectedIndex = navigationEntries.indexWhere((element) {
+      return element.route == routerUri;
     });
+
     return StoreConnector<AppState, NavigationViewModel>(
       vm: () => NavigationStateFactory(),
       builder: (context, vm) {
         return NavigationRail(
           minExtendedWidth: maxXOffset,
-          extended:
-          MediaQuery.of(context).size.width < 1000 ? false : vm.openNavigation,
-          onDestinationSelected: (index) {
-            context.go(navigationEntries[index].route);
-          },
+          extended: MediaQuery.of(context).size.width >= 1000 ? vm.openNavigation : false,
+          onDestinationSelected: (index) => _onDestinationSelected(context, index),
           labelType: NavigationRailLabelType.none,
           destinations: _buildNavigationView(),
-          selectedIndex: index != -1 ? index : 0,
+          selectedIndex: selectedIndex != -1 ? selectedIndex : 0,
         );
       },
     );
   }
 
+  /// Handles the selection of a navigation destination.
+  void _onDestinationSelected(BuildContext context, int index) {
+    context.go(navigationEntries[index].route);
+  }
+
+  /// Builds the list of navigation destinations for the [NavigationRail].
   List<NavigationRailDestination> _buildNavigationView() {
     return List.generate(
       navigationEntries.length,
-      (index) {
+          (index) {
         final navigationValue = navigationEntries[index];
         return NavigationRailDestination(
           selectedIcon: Icon(navigationValue.selected),
