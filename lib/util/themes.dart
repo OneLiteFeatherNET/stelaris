5066793f15c7edacc9538a82e7e1dc2b90fe492e
theEvilReaper <steffenwx@gmail.com>
2025-02-24 18:59:43 +0100

    Remove useless useMaterial3 option

--- lib/util/themes.dart
+++ lib/util/themes.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 
 var lightMode = ThemeData(
   brightness: Brightness.light,
-  useMaterial3: true,
   colorSchemeSeed: Colors.green[400],
   secondaryHeaderColor: Colors.green[200],
 );
 
 var darkMode = ThemeData(
   brightness: Brightness.dark,
-  useMaterial3: true,
   colorSchemeSeed: Colors.teal[400],
   secondaryHeaderColor: Colors.teal[800],
 );
