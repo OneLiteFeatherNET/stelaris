87e4813c955712c452f01c19c9df6a5eb24f16d4
theEvilReaper <steffenwx@gmail.com>
2025-03-23 11:10:14 +0100

    Read urls from the environment

--- lib/env/environment.dart
+++ lib/env/environment.dart
@@ -1,6 +1,8 @@
 /// This file contains the environment variables for the app
 /// The pipeline should replace this constant with the right data
 class Environment {
-  static const String backendURl = '';
-  static const String generatorUrl = '';
+  static const String backendURl =
+      String.fromEnvironment('BACKEND_URL', defaultValue: '');
+  static const String generatorUrl =
+      String.fromEnvironment('GENERATOR_URL', defaultValue: '');
 }
