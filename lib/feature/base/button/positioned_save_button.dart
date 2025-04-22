0ffc5f8b8486c2ee9c0e29dca8b91dd384272f03
theEvilReaper <steffenwx@gmail.com>
2025-04-22 16:15:54 +0200

    Replace cupertino with material

--- lib/feature/base/button/positioned_save_button.dart
+++ lib/feature/base/button/positioned_save_button.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:stelaris/feature/base/button/save_button.dart';
 
 class PositionedSaveButton extends StatelessWidget {
