c36cebc03a155c1579cb3bf24265c196be20252d
theEvilReaper <steffenwx@gmail.com>
2025-06-12 10:53:59 +0200

    Improve sound state classes

--- lib/api/state/factory/sound/selected_sound_state.dart
+++ lib/api/state/factory/sound/selected_sound_state.dart
@@ -1,9 +1,10 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:stelaris/api/model/sound/sound_event_model.dart';
 import 'package:stelaris/api/state/app_state.dart';
+import 'package:stelaris/feature/sound/sound_general_page.dart';
 
 class SelectedSoundState
-    extends VmFactory<AppState, SoundPage, SelectedSoundView> {
+    extends VmFactory<AppState, SoundGeneralPage, SelectedSoundView> {
   SelectedSoundState();
 
   @override
