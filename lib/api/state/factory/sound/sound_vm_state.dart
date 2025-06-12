73aaa77ed88d1b01356282960f6d1836548dbb52
theEvilReaper <steffenwx@gmail.com>
2025-06-12 08:30:34 +0200

    Add more state management related code for sounds

---
+++ lib/api/state/factory/sound/sound_vm_state.dart
@@ -0,0 +1,32 @@
+import 'package:async_redux/async_redux.dart';
+import 'package:stelaris/api/model/sound/sound_event_model.dart';
+import 'package:stelaris/api/state/app_state.dart';
+
+class SoundVmFactory extends VmFactory<AppState, SoundPage, SoundViewModel> {
+  SoundVmFactory();
+
+  @override
+  SoundViewModel fromStore() => SoundViewModel(
+    models: state.soundEvents,
+    selected: state.selectedSoundEvent,
+  );
+}
+
+class SoundViewModel extends Vm {
+  SoundViewModel({required this.models, required this.selected})
+    : super(equals: [selected, models]);
+
+  final SoundEventModel? selected;
+  final List<SoundEventModel> models;
+
+  bool isSelectedItem(SoundEventModel model) {
+    if (selected == null) return false;
+
+    final selectedModel = selected!;
+
+    if (selectedModel.id != null && model.id != null) {
+      return selectedModel.id == model.id;
+    }
+    return selectedModel.hashCode == model.hashCode;
+  }
+}
