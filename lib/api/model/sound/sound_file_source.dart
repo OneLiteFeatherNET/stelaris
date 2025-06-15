cd4b754423956475123e79a6f89fd27d6f75fe51
theEvilReaper <steffenwx@gmail.com>
2025-06-15 12:54:18 +0200

    Update SoundFileSource class

--- lib/api/model/sound/sound_file_source.dart
+++ lib/api/model/sound/sound_file_source.dart
@@ -6,20 +6,18 @@ part 'sound_file_source.g.dart';
 
 @freezed
 abstract class SoundFileSource with _$SoundFileSource, DataModel {
-
   const SoundFileSource._();
 
   const factory SoundFileSource({
     required String name,
+    required double volume,
+    required double pitch,
+    required int attenuationDistance,
+    required bool preload,
+    required String type,
+    required int weight,
     String? id,
-    @Default(1.0) double volume,
-    @Default(1.0) double pitch,
-    int? weight,
-    @Default(16) int attenuationDistance,
-    @Default(false) bool preload,
-    @Default('file') String type
-}) = _SoundFileSource;
-
+  }) = _SoundFileSource;
 
   factory SoundFileSource.fromJson(Map<String, dynamic> json) =>
       _$SoundFileSourceFromJson(json);
--- lib/api/model/sound/sound_file_source.g.dart
+++ lib/api/model/sound/sound_file_source.g.dart
@@ -9,23 +9,23 @@ part of 'sound_file_source.dart';
 _SoundFileSource _$SoundFileSourceFromJson(Map<String, dynamic> json) =>
     _SoundFileSource(
       name: json['name'] as String,
+      volume: (json['volume'] as num).toDouble(),
+      pitch: (json['pitch'] as num).toDouble(),
+      attenuationDistance: (json['attenuationDistance'] as num).toInt(),
+      preload: json['preload'] as bool,
+      type: json['type'] as String,
+      weight: (json['weight'] as num).toInt(),
       id: json['id'] as String?,
-      volume: (json['volume'] as num?)?.toDouble() ?? 1.0,
-      pitch: (json['pitch'] as num?)?.toDouble() ?? 1.0,
-      weight: (json['weight'] as num?)?.toInt(),
-      attenuationDistance: (json['attenuationDistance'] as num?)?.toInt() ?? 16,
-      preload: json['preload'] as bool? ?? false,
-      type: json['type'] as String? ?? 'file',
     );
 
 Map<String, dynamic> _$SoundFileSourceToJson(_SoundFileSource instance) =>
     <String, dynamic>{
       'name': instance.name,
-      'id': instance.id,
       'volume': instance.volume,
       'pitch': instance.pitch,
-      'weight': instance.weight,
       'attenuationDistance': instance.attenuationDistance,
       'preload': instance.preload,
       'type': instance.type,
+      'weight': instance.weight,
+      'id': instance.id,
     };
