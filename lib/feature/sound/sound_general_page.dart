61b1dcc586b6c501a37033d39198967bfa0d750b
Joltras <MinnyMouseXD@gmail.com>
2025-06-15 16:29:04 +0200

    Remove Expanded usage to avoid a stacktrace

--- lib/feature/sound/sound_general_page.dart
+++ lib/feature/sound/sound_general_page.dart
@@ -46,129 +46,127 @@ class _SoundGeneralPageState extends State<SoundGeneralPage> {
       onDispose: (store) =>
           store.dispatch(RemoveSelectedSoundEvent(), notify: false),
       builder: (context, vm) {
-        return Expanded(
-          child: Form(
-            key: _key,
-            autovalidateMode: AutovalidateMode.onUserInteraction,
-            child: Stack(
-              children: [
-                Positioned.fill(
-                  child: AnimatedOpacity(
-                    duration: const Duration(milliseconds: 300),
-                    opacity: 1,
-                    child: LayoutBuilder(
-                      builder: (context, constraints) {
-                        return Scrollbar(
+        return Form(
+          key: _key,
+          autovalidateMode: AutovalidateMode.onUserInteraction,
+          child: Stack(
+            children: [
+              Positioned.fill(
+                child: AnimatedOpacity(
+                  duration: const Duration(milliseconds: 300),
+                  opacity: 1,
+                  child: LayoutBuilder(
+                    builder: (context, constraints) {
+                      return Scrollbar(
+                        controller: _scrollController,
+                        thumbVisibility: true,
+                        trackVisibility: true,
+                        child: SingleChildScrollView(
                           controller: _scrollController,
-                          thumbVisibility: true,
-                          trackVisibility: true,
-                          child: SingleChildScrollView(
-                            controller: _scrollController,
-                            child: Padding(
-                              padding: const EdgeInsets.all(16),
-                              child: Wrap(
-                                spacing: 16,
-                                runSpacing: 16,
-                                children: [
-                                  TextInputCard<String>(
-                                    display: context.l10n.card_name,
-                                    currentValue:
-                                        vm.selected.variableName ?? emptyString,
-                                    formatter: [
-                                      FilteringTextInputFormatter.allow(
-                                        stringPattern,
-                                      ),
-                                    ],
-                                    valueUpdate: (value) {
-                                      if (value != vm.selected.variableName) {
-                                        final oldModel = vm.selected;
-                                        final newEntry = oldModel.copyWith(
-                                          variableName: value,
-                                        );
-                                        context.dispatch(
-                                          UpdateSoundAction(newEntry),
-                                        );
-                                      }
-                                    },
-                                    formValidator: (value) {
-                                      if (value.trim().isEmpty) {
-                                        return context.l10n.error_card_empty;
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                  TextInputCard<String>(
-                                    display: 'Key',
-                                    currentValue:
-                                        vm.selected.keyName ?? emptyString,
-                                    formatter: [
-                                      FilteringTextInputFormatter.allow(
-                                        stringPattern,
-                                      ),
-                                    ],
-                                    valueUpdate: (value) {
-                                      if (value != vm.selected.keyName) {
-                                        final oldModel = vm.selected;
-                                        final newEntry = oldModel.copyWith(
-                                          keyName: value,
-                                        );
-                                        context.dispatch(
-                                          UpdateSoundAction(newEntry),
-                                        );
-                                      }
-                                    },
-                                    formValidator: (value) {
-                                      if (value.trim().isEmpty) {
-                                        return context.l10n.error_card_empty;
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                  TextInputCard<String>(
-                                    display: 'Subtitle',
-                                    currentValue:
-                                        vm.selected.subTitle ?? emptyString,
-                                    formatter: [
-                                      FilteringTextInputFormatter.allow(
-                                        stringPattern,
-                                      ),
-                                    ],
-                                    valueUpdate: (value) {
-                                      if (value != vm.selected.subTitle) {
-                                        final oldModel = vm.selected;
-                                        final newEntry = oldModel.copyWith(
-                                          subTitle: value,
-                                        );
-                                        context.dispatch(
-                                          UpdateSoundAction(newEntry),
-                                        );
-                                      }
-                                    },
-                                    formValidator: (value) {
-                                      if (value.trim().isEmpty) {
-                                        return context.l10n.error_card_empty;
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                ],
-                              ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16),
+                            child: Wrap(
+                              spacing: 16,
+                              runSpacing: 16,
+                              children: [
+                                TextInputCard<String>(
+                                  display: context.l10n.card_name,
+                                  currentValue:
+                                      vm.selected.variableName ?? emptyString,
+                                  formatter: [
+                                    FilteringTextInputFormatter.allow(
+                                      stringPattern,
+                                    ),
+                                  ],
+                                  valueUpdate: (value) {
+                                    if (value != vm.selected.variableName) {
+                                      final oldModel = vm.selected;
+                                      final newEntry = oldModel.copyWith(
+                                        variableName: value,
+                                      );
+                                      context.dispatch(
+                                        UpdateSoundAction(newEntry),
+                                      );
+                                    }
+                                  },
+                                  formValidator: (value) {
+                                    if (value.trim().isEmpty) {
+                                      return context.l10n.error_card_empty;
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                TextInputCard<String>(
+                                  display: 'Key',
+                                  currentValue:
+                                      vm.selected.keyName ?? emptyString,
+                                  formatter: [
+                                    FilteringTextInputFormatter.allow(
+                                      stringPattern,
+                                    ),
+                                  ],
+                                  valueUpdate: (value) {
+                                    if (value != vm.selected.keyName) {
+                                      final oldModel = vm.selected;
+                                      final newEntry = oldModel.copyWith(
+                                        keyName: value,
+                                      );
+                                      context.dispatch(
+                                        UpdateSoundAction(newEntry),
+                                      );
+                                    }
+                                  },
+                                  formValidator: (value) {
+                                    if (value.trim().isEmpty) {
+                                      return context.l10n.error_card_empty;
+                                    }
+                                    return null;
+                                  },
+                                ),
+                                TextInputCard<String>(
+                                  display: 'Subtitle',
+                                  currentValue:
+                                      vm.selected.subTitle ?? emptyString,
+                                  formatter: [
+                                    FilteringTextInputFormatter.allow(
+                                      stringPattern,
+                                    ),
+                                  ],
+                                  valueUpdate: (value) {
+                                    if (value != vm.selected.subTitle) {
+                                      final oldModel = vm.selected;
+                                      final newEntry = oldModel.copyWith(
+                                        subTitle: value,
+                                      );
+                                      context.dispatch(
+                                        UpdateSoundAction(newEntry),
+                                      );
+                                    }
+                                  },
+                                  formValidator: (value) {
+                                    if (value.trim().isEmpty) {
+                                      return context.l10n.error_card_empty;
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ],
                             ),
                           ),
-                        );
-                      },
-                    ),
+                        ),
+                      );
+                    },
                   ),
                 ),
-                PositionedSaveButton.standard(
-                  callback: () {
-                    if (_key.currentState?.validate() ?? false) {
-                      context.dispatch(SoundDatabaseUpdate());
-                    }
-                  },
-                ),
-              ],
-            ),
+              ),
+              PositionedSaveButton.standard(
+                callback: () {
+                  if (_key.currentState?.validate() ?? false) {
+                    context.dispatch(SoundDatabaseUpdate());
+                  }
+                },
+              ),
+            ],
           ),
         );
       },
