531b93c539c5dc8049892c2d41e226015cc89b1b
Joltras <MinnyMouseXD@gmail.com>
2025-06-17 21:14:42 +0200

    Add documentation

--- lib/api/state/actions/app_actions.dart
+++ lib/api/state/actions/app_actions.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 
 import 'package:async_redux/async_redux.dart';
+import 'package:flutter/material.dart';
 import 'package:stelaris/api/state/app_state.dart';
 
+/// The [UpdateNavigationAction] is a [ReduxAction] that toggles the navigation state in the [AppState] of the application.
+/// The navigation state indicates if the [NavigationDrawer] is open or closed.
 class UpdateNavigationAction extends ReduxAction<AppState> {
-
   @override
   Future<AppState> reduce() async {
     return state.copyWith(openNavigation: !state.openNavigation);
