722e364584f5ede6a146ba6f568af92e9d8919f5
theEvilReaper <theevilreaper@users.noreply.github.com>
2025-08-12 10:22:10 +0200

    Improve vm state for the selected sound

--- lib/api/state/factory/sound/selected_sound_state.dart
+++ lib/api/state/factory/sound/selected_sound_state.dart
@@ -1,6 +1,9 @@
 import 'package:async_redux/async_redux.dart';
+import 'package:flutter/foundation.dart';
 import 'package:stelaris/api/model/sound/sound_event_model.dart';
 import 'package:stelaris/api/model/sound/sound_file_source.dart';
+import 'package:stelaris/api/paginated_result.dart';
+import 'package:stelaris/api/state/actions/sound/sound_file_actions.dart';
 import 'package:stelaris/api/state/app_state.dart';
 import 'package:stelaris/feature/sound/sound_general_page.dart';
 
@@ -11,8 +14,23 @@ class SelectedSoundState
   SelectedSoundState();
 
   @override
-  SelectedSoundView fromStore() =>
-      SelectedSoundView(selected: state.selectedSoundEvent!);
+  SelectedSoundView fromStore() {
+    final selectedEvent = state.selectedSoundEvent;
+
+    return SelectedSoundView(
+      selected: state.selectedSoundEvent!,
+      onLoadMoreSoundFiles: () {
+        if (selectedEvent != null &&
+            selectedEvent.id != null &&
+            selectedEvent.files.hasNextPage &&
+            !selectedEvent.isLoading) {
+          dispatch(
+            LoadMoreSoundFiles(pageToLoad: selectedEvent.files.currentPage + 1),
+          );
+        }
+      },
+    );
+  }
 }
 
 /// The [SelectedSoundView] is a view model that represents the currently selected sound event.
@@ -20,18 +38,31 @@ class SelectedSoundState
 ///
 /// It includes additional methods to help determine if the selected sound event has associated sound files
 class SelectedSoundView extends Vm {
-  SelectedSoundView({required this.selected}) : super(equals: [selected]);
+  SelectedSoundView({
+    required this.selected,
+    required this.onLoadMoreSoundFiles,
+  }) : super(equals: [selected, selected.files.items.length]);
 
   final SoundEventModel selected;
+  final VoidCallback onLoadMoreSoundFiles;
 
   /// Returns a boolean indicator if the selected sound event contains sound files or not.
-  bool get hasNoFiles => selected.files == null || selected.files!.isEmpty;
+  bool get hasNoFiles => selected.files.items.isEmpty;
+
+  /// Internal variable to hold the paginated result of sound files
+  PaginatedResult<SoundFileSource> get _fileResults => selected.files;
 
   /// Returns a list of sound file sources associated with the selected sound event.
-  List<SoundFileSource> get sources => selected.files ?? <SoundFileSource>[];
+  List<SoundFileSource> get sources => _fileResults.items;
 
   /// Returns the count of sound files associated with the selected sound event.
-  int get fileCount => selected.files?.length ?? 0;
+  int get fileCount => selected.files.items.length;
+
+  /// Indication if there are more pages of sound files to load
+  bool get hasNextPage => _fileResults.hasNextPage;
+
+  /// Returns the current page of sound files associated with the selected sound event.
+  bool get isLoadingFiles => selected.isLoading;
 
   /// Provides access to a specific sound file source by its index.
   /// If the index is out of bounds, it throws a [RangeError].
@@ -39,6 +70,6 @@ class SelectedSoundView extends Vm {
     if (index < 0 || index >= fileCount) {
       throw RangeError.index(index, sources, 'index', null, fileCount);
     }
-    return sources[index]; // Example usage, can be modified as needed
+    return sources[index];
   }
 }
