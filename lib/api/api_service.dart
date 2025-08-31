67b8dd3f2c5bac6310c9b840ecb454066c6978f0
Phillipp Glanz <p.glanz@madfix.me>
2025-08-31 16:45:51 +0200

    refactor(api): Replace ApiClient with VulpesBackendClient and update API endpoints

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,12 +1,7 @@
 import 'package:stelaris/api/api_client.dart';
-import 'package:stelaris/api/base_api.dart';
-import 'package:stelaris/api/client_api.dart';
-import 'package:stelaris/api/model/attribute_model.dart';
-import 'package:stelaris/api/model/font_model.dart';
-import 'package:stelaris/api/model/item_model.dart';
-import 'package:stelaris/api/model/notification_model.dart';
 import 'package:stelaris/api/service/generate_api.dart';
 import 'package:stelaris/env/environment.dart';
+import 'package:vulpes_backend_client/vulpes_backend_client.dart';
 
 /// The [ApiService] class contains all web services which are used in the app to communicate with the backend.
 class ApiService {
@@ -16,42 +11,22 @@ class ApiService {
 
   ApiService._internal();
 
-  late final ApiClient _apiClient = _createApiClient();
+  late final VulpesBackendClient _apiClient = _createApiClient();
 
   late final ApiClient _generatorClient = _createGeneratorClient();
 
   late final GenerateApi generateApi = GenerateApi(_generatorClient);
 
-  late final ClientAPI<ItemModel> itemApi = BaseApi(
-    apiClient: _apiClient,
-    endpoint: 'item',
-    fromJson: (p0) => ItemModel.fromJson(p0),
-    toJson: (model) => model.toJson(),
-  );
+  late final ItemApi itemApi = _apiClient.getItemApi();
 
-  late final ClientAPI<NotificationModel> notificationApi = BaseApi(
-    apiClient: _apiClient,
-    endpoint: 'notification',
-    fromJson: (p0) => NotificationModel.fromJson(p0),
-    toJson: (model) => model.toJson(),
-  );
+  late final NotificationApi notificationApi = _apiClient.getNotificationApi();
 
-  late final ClientAPI<FontModel> fontApi = BaseApi(
-    apiClient: _apiClient,
-    endpoint: 'font',
-    fromJson: (p0) => FontModel.fromJson(p0),
-    toJson: (model) => model.toJson(),
-  );
+  late final FontApi fontApi = _apiClient.getFontApi();
 
-  late final ClientAPI<AttributeModel> attributeApi = BaseApi(
-    apiClient: _apiClient,
-    endpoint: 'attribute',
-    fromJson: (p0) => AttributeModel.fromJson(p0),
-    toJson: (model) => model.toJson(),
-  );
+  late final AttributeApi attributeApi = _apiClient.getAttributeApi();
 
   /// Creates an instance of [ApiClient] with the backend URL.
-  ApiClient _createApiClient() => ApiClient(Environment.backendURl);
+  VulpesBackendClient _createApiClient() => VulpesBackendClient(basePathOverride: Environment.backendURl);
 
   /// Creates an instance of [ApiClient] with the generator URL.
   ApiClient _createGeneratorClient() => ApiClient(Environment.generatorUrl);
