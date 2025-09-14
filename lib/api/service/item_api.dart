e0de227af08d04a8d4228525ffc861793e967e7b
theEvilReaper <theevilreaper@users.noreply.github.com>
2025-09-14 11:19:07 +0200

    Add new endpoint to fetch enchantments, lore and flags from an item

--- lib/api/service/item_api.dart
+++ lib/api/service/item_api.dart
@@ -1,4 +1,7 @@
 import 'package:stelaris/api/base_api.dart';
+import 'package:stelaris/api/model/item/item_enchantment_model.dart';
+import 'package:stelaris/api/model/item/item_flag_model.dart';
+import 'package:stelaris/api/model/item/item_lore_model.dart';
 import 'package:stelaris/api/model/item_model.dart';
 
 class ItemAPI extends BaseApi<ItemModel> {
@@ -10,4 +13,24 @@ class ItemAPI extends BaseApi<ItemModel> {
     toJson: (model) => model.toJson(),
   );
 
+  Future<ItemEnchantmentModel> getEnchantments(String id) async {
+    final baseUri = Uri.parse(apiClient.baseUrl);
+    final uri = baseUri.replace(path: '${baseUri.path}/$endpoint/enchantments/$id');
+    final result = await apiClient.dio.getUri(uri);
+    return ItemEnchantmentModel.fromJson(result.data!);
+  }
+
+  Future<ItemLoreModel> getLore(String id) async {
+    final baseUri = Uri.parse(apiClient.baseUrl);
+    final uri = baseUri.replace(path: '${baseUri.path}/$endpoint/lore/$id');
+    final result = await apiClient.dio.getUri(uri);
+    return ItemLoreModel.fromJson(result.data!);
+  }
+
+  Future<ItemFlagModel> getFlags(String id) async {
+    final baseUri = Uri.parse(apiClient.baseUrl);
+    final uri = baseUri.replace(path: '${baseUri.path}/$endpoint/flags/$id');
+    final result = await apiClient.dio.getUri(uri);
+    return ItemFlagModel.fromJson(result.data!);
+  }
 }
