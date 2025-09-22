bfd47e2388499456dfd49ea38d537f7ddcf96eff
theEvilReaper <theevilreaper@users.noreply.github.com>
2025-09-22 23:19:29 +0200

    Add input section widget for strings

---
+++ lib/feature/sound/modal/section/string_field_section.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:stelaris/feature/sound/modal/section/base_section.dart';
+
+class StringInputSection extends StatefulWidget {
+  const StringInputSection({super.key});
+
+  @override
+  State<StringInputSection> createState() => _StringInputSectionState();
+}
+
+class _StringInputSectionState extends State<StringInputSection> {
+  late final TextEditingController _controller;
+  final _borderRadius = BorderRadius.circular(8);
+
+  @override
+  void initState() {
+    super.initState();
+    this._controller = TextEditingController();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final outlineBorder = OutlineInputBorder(
+      borderRadius: _borderRadius,
+      borderSide: BorderSide(
+        color: colorScheme.outline,
+      ),
+    );
+    return BaseSection(
+      child: TextField(
+        controller: _controller,
+        decoration: InputDecoration(
+          hintText: 'Enter your sound name',
+          hintStyle: TextStyle(color: colorScheme.onSurfaceVariant),
+          border: outlineBorder,
+          suffixIcon: const Tooltip(
+            message: 'test',
+            child: const Icon(Icons.info_outline),
+          ),
+          enabledBorder: outlineBorder,
+          focusedBorder: OutlineInputBorder(
+            borderRadius: _borderRadius,
+            borderSide: BorderSide(color: colorScheme.primary, width: 2),
+          ),
+          errorBorder: OutlineInputBorder(
+            borderRadius: _borderRadius,
+            borderSide: BorderSide(color: colorScheme.error),
+          ),
+          focusedErrorBorder: OutlineInputBorder(
+            borderRadius: _borderRadius,
+            borderSide: BorderSide(color: colorScheme.error, width: 2),
+          ),
+          contentPadding: const EdgeInsets.symmetric(
+            horizontal: 12,
+            vertical: 8,
+          ),
+        ),
+      ),
+    );
+  }
+}
