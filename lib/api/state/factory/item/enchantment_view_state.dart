ca3532804332501bf07e740ae0337e4e24e533eb
theEvilReaper <theevilreaper@users.noreply.github.com>
2025-11-18 08:11:59 +0100

    Improve enchantment view state

--- lib/api/state/factory/item/enchantment_view_state.dart
+++ lib/api/state/factory/item/enchantment_view_state.dart
@@ -12,28 +12,37 @@ class EnchantmentViewFactory
 
   @override
   EnchantmentView fromStore() {
-    return EnchantmentView(
-      selected: state.selectedItem!,
-    );
+    return EnchantmentView(selected: state.selectedItem!);
   }
 }
 
 class EnchantmentView extends Vm with EnchantmentReducer {
-  EnchantmentView({
-    required this.selected,
-  }) : super(equals: [selected]);
+  EnchantmentView({required this.selected}) : super(equals: [selected]);
 
   final ItemModel selected;
 
   /// A lookup map of selected enchantments for efficient access.
-  Map<String, ItemEnchantmentDto> get selectedEnchantmentMap =>
-      {for (var e in selected.enchantments.items) e.name: e};
+  Map<String, ItemEnchantmentDto> get selectedEnchantmentMap => {
+    for (var e in selected.enchantments.items) e.name: e,
+  };
 
   /// A filtered list of active enchantments based on what's been selected.
   List<Enchantment> get activeEnchantments {
     final allEnchantments = getEnchantments(selected);
     return allEnchantments
-        .where((ench) => selectedEnchantmentMap.containsKey(ench.minecraftValue))
+        .where(
+          (ench) => selectedEnchantmentMap.containsKey(ench.minecraftValue),
+        )
         .toList();
   }
+
+  /// Overwrites the default index operator to access the active enchantments
+  /// [index] the index to access the data
+  Enchantment operator [](int index) => activeEnchantments[index];
+
+  /// Returns true if there are any active enchantments
+  bool get hasEnchantments => activeEnchantments.isNotEmpty;
+
+  /// Returns true if there are more enchantments to load
+  bool get isLoadingMore => selected.isLoadingMoreEnchantments;
 }
