32fe06afc5dc003840dfdd3611ca0036976df738
theEvilReaper <theevilreaper@users.noreply.github.com>
2025-11-21 11:16:57 +0100

    Add enchantment save and refactor nullable usage

--- lib/feature/item/enchantment/dialog/item_enchantments_dialog.dart
+++ lib/feature/item/enchantment/dialog/item_enchantments_dialog.dart
@@ -1,21 +1,24 @@
+import 'package:async_redux/async_redux.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:stelaris/api/model/item/item_enchantment_dto.dart';
 import 'package:stelaris/api/model/item_model.dart';
+import 'package:stelaris/api/state/actions/item/item_enchantment_actions.dart';
+import 'package:stelaris/api/state/factory/item/enchantment_view_state.dart';
 import 'package:stelaris/feature/item/enchantment_reducer.dart';
 import 'package:stelaris/util/l10n_ext.dart';
 import 'package:stelaris/util/constants.dart';
-import 'package:stelaris/util/typedefs.dart';
 import 'package:vulpes_data/api/enchantment.dart';
 
 class ItemEnchantmentAddDialog extends StatefulWidget {
   const ItemEnchantmentAddDialog({
-    required this.addEnchantmentCallback,
     required this.model,
+    required this.view,
     super.key,
   });
 
-  final AddEnchantmentCallback addEnchantmentCallback;
   final ItemModel model;
+  final EnchantmentView view;
 
   @override
   State<ItemEnchantmentAddDialog> createState() =>
@@ -25,8 +28,10 @@ class ItemEnchantmentAddDialog extends StatefulWidget {
 class _ItemEnchantmentAddDialogState extends State<ItemEnchantmentAddDialog>
     with EnchantmentReducer {
   final TextEditingController _controller = TextEditingController();
-  final ValueNotifier<Enchantment?> _selected = ValueNotifier(null);
+  final ValueNotifier<bool> _unsafe = ValueNotifier(false);
   final _key = GlobalKey<FormState>();
+
+  late final ValueNotifier<Enchantment> _selected;
   late List<DropdownMenuItem<Enchantment>> _enchantments;
 
   @override
@@ -44,27 +49,11 @@ class _ItemEnchantmentAddDialogState extends State<ItemEnchantmentAddDialog>
     }
   }
 
-  void _updateEnchantments() {
-    _enchantments = getEnchantments(widget.model, true)
-        .map(
-          (e) =>
-          DropdownMenuItem<Enchantment>(
-            value: e,
-            child: Text(e.displayName),
-          ),
-    )
-        .toList();
-    if (_enchantments.isNotEmpty) {
-      _selected.value = _enchantments[0].value;
-    } else {
-      _selected.value = null;
-    }
-  }
-
   @override
   void dispose() {
     _controller.dispose();
     _selected.dispose();
+    _unsafe.dispose();
     super.dispose();
   }
 
@@ -79,15 +68,15 @@ class _ItemEnchantmentAddDialogState extends State<ItemEnchantmentAddDialog>
       children: [
         Text(context.l10n.dialog_enchantment_enchantment),
         horizontalSpacing10,
-        ValueListenableBuilder<Enchantment?>(
+        ValueListenableBuilder<Enchantment>(
           valueListenable: _selected,
           builder: (context, selectedEnchantment, child) {
-            return DropdownButtonFormField<Enchantment?>(
+            return DropdownButtonFormField<Enchantment>(
               autofocus: true,
               initialValue: selectedEnchantment,
               items: _enchantments,
               onChanged: (value) {
-                _selected.value = value;
+                _selected.value = value!;
                 _resetController();
               },
             );
@@ -95,6 +84,22 @@ class _ItemEnchantmentAddDialogState extends State<ItemEnchantmentAddDialog>
         ),
         verticalSpacing25,
         Text(context.l10n.label_level),
+        verticalSpacing25,
+        ValueListenableBuilder<bool>(
+          valueListenable: _unsafe,
+          builder: (context, unsafe, child) {
+            return CheckboxListTile(
+              title: const Text('Unsafe'),
+              value: unsafe,
+              onChanged: (value) {
+                _unsafe.value = value ?? false;
+                _key.currentState?.validate();
+              },
+              controlAffinity: ListTileControlAffinity.leading,
+              contentPadding: EdgeInsets.zero,
+            );
+          },
+        ),
         Form(
           key: _key,
           autovalidateMode: AutovalidateMode.always,
@@ -107,7 +112,9 @@ class _ItemEnchantmentAddDialogState extends State<ItemEnchantmentAddDialog>
               if (value == null) return null;
               return _validateInput(
                 value: value,
-                maxLevel: _selected.value?.maxLevel ?? 1,);
+                maxLevel: _selected.value.maxLevel,
+                unsafe: _unsafe.value,
+              );
             },
           ),
         ),
@@ -115,12 +122,8 @@ class _ItemEnchantmentAddDialogState extends State<ItemEnchantmentAddDialog>
         TextButton(
           onPressed: () {
             if (!_key.currentState!.validate()) return;
-            if (_selected.value == null) return;
-            widget.addEnchantmentCallback(
-              _selected.value!,
-              int.parse(_controller.value.text),
-            );
-            _selected.value = null;
+            final int level = int.parse(_controller.value.text);
+            _handleAddCallback(_selected.value, level, _unsafe.value);
           },
           child: Text(context.l10n.button_add),
         ),
@@ -134,11 +137,42 @@ class _ItemEnchantmentAddDialogState extends State<ItemEnchantmentAddDialog>
     }
   }
 
+  void _updateEnchantments() {
+    _enchantments = widget.view.selectableEnchantments
+        .map(
+          (e) => DropdownMenuItem<Enchantment>(
+        value: e,
+        child: Text(e.displayName),
+      ),
+    )
+        .toList();
+    if (_enchantments.isNotEmpty) {
+      _selected = ValueNotifier(_enchantments.first.value!);
+    }
+  }
+
+  /// Handles the add logic of an selected [Enchantment] with the given data
+  /// It calls the [ItemEnchantmentAddAction] to save it in the database etc.
+  /// [enchantment] which should be added to the [ItemModel]
+  /// [level] the level which should be used
+  /// [unsafe] indication if the [Enchantment] is unsafe or not
+  void _handleAddCallback(Enchantment enchantment, int level, bool unsafe) {
+    final ItemEnchantmentDto dto = ItemEnchantmentDto(
+      name: enchantment.minecraftValue,
+      level: level,
+      unsafe: unsafe,
+    );
+    context.dispatch(ItemEnchantmentAddAction(dto));
+    Navigator.of(context).pop();
+  }
+
   ///
-  String? _validateInput({required String value, required int maxLevel}) {
-    if (value
-        .trim()
-        .isEmpty) {
+  String? _validateInput({
+    required String value,
+    required int maxLevel,
+    required bool unsafe,
+  }) {
+    if (value.trim().isEmpty) {
       return 'Please enter a level';
     }
     final level = int.tryParse(value);
@@ -146,7 +180,7 @@ class _ItemEnchantmentAddDialogState extends State<ItemEnchantmentAddDialog>
       return 'Please enter a valid number';
     }
 
-    if (level > maxLevel) {
+    if (!unsafe && level > maxLevel) {
       return 'The maximum is $maxLevel';
     }
 
