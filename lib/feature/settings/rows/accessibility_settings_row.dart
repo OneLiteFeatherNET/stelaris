57838a636d9300ec8924545e2b118bc72162456b
theEvilReaper <theevilreaper@users.noreply.github.com>
2025-11-30 15:02:49 +0100

    Replace row usage with the SettingsItem

--- lib/feature/settings/rows/accessibility_settings_row.dart
+++ lib/feature/settings/rows/accessibility_settings_row.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:stelaris/feature/settings/settings_base_row.dart';
-import 'package:stelaris/feature/settings/settings_text_tile.dart';
+import 'package:stelaris/feature/settings/settings_item.dart';
 import 'package:stelaris/util/constants.dart';
 import 'package:stelaris/util/l10n_ext.dart';
 import 'package:stelaris/util/url_launcher.dart';
@@ -12,20 +12,13 @@ class AccessibilitySettingsRow extends StatelessWidget {
   Widget build(BuildContext context) {
     return SettingsBaseRow(
       title: context.l10n.settings_accessibility_title,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Expanded(
-            child: SettingsTextTile(
-              headerLine: context.l10n.settings_accessibility_body,
-              bodyLine: context.l10n.settings_accessibility_header,
-            ),
-          ),
-          OutlinedButton(
-            onPressed: () => UriLauncher.launchURL(conceptURL),
-            child: Text(context.l10n.settings_accessibility_button),
-          ),
-        ],
+      child: SettingsItem(
+        title: context.l10n.settings_accessibility_body,
+        subtitle: context.l10n.settings_accessibility_header,
+        trailing: OutlinedButton(
+          onPressed: () => UriLauncher.launchURL(conceptURL),
+          child: Text(context.l10n.settings_accessibility_button),
+        ),
       ),
     );
   }
