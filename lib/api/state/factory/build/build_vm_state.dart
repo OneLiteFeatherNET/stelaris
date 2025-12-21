c5a74abc53df790ffa09e078cc9afc8a9baa8aa6
Joltras <minnymousexd@gmail.com>
2025-12-21 21:44:17 +0100

    chore(state): improve build view state

--- lib/api/state/factory/build/build_vm_state.dart
+++ lib/api/state/factory/build/build_vm_state.dart
@@ -1,20 +1,27 @@
 import 'package:async_redux/async_redux.dart';
 import 'package:stelaris/api/model/release/release_model.dart';
 import 'package:stelaris/api/state/app_state.dart';
-import 'package:stelaris/feature/build/build_drawer.dart';
+import 'package:stelaris/feature/build/build_dialog.dart';
 
-class BuildStateFactory extends VmFactory<AppState, BuildDrawer, BuildViewModel> {
+class BuildStateFactory extends VmFactory<AppState, BuildDialog, BuildViewModel> {
 
   BuildStateFactory();
 
   @override
-  BuildViewModel fromStore() => BuildViewModel(releaseModel: state.releaseModel);
+  BuildViewModel fromStore() => BuildViewModel(
+        releaseModel: state.releaseModel,
+        branches: state.branches,
+      );
 }
 
 class BuildViewModel extends Vm {
-
-  BuildViewModel({required this.releaseModel}) : super(equals: [releaseModel]);
+  BuildViewModel({
+    required this.releaseModel,
+    required this.branches,
+  }) : super(equals: [releaseModel, branches]);
 
   final ReleaseModel? releaseModel;
+  final List<String> branches;
 
+  String get version => releaseModel == null ? '0.0.1' : releaseModel!.version;
 }
