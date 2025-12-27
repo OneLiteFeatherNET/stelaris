4ed59a6122e2018021d91732ac444d9542a17a62
theEvilReaper <theevilreaper@users.noreply.github.com>
2025-12-27 11:52:06 +0100

    chore(release): add comment for variable naming

--- lib/api/model/release/release_model.dart
+++ lib/api/model/release/release_model.dart
@@ -14,6 +14,7 @@ abstract class ReleaseModel with _$ReleaseModel {
     required DateTime publishedAt,
     String? url,
     @Default(false) bool prerelease,
+    // The naming of this comes from the official api https://docs.github.com/en/rest/releases/releases. Search for target_commitish
     String? targetCommitish,
   }) = _ReleaseModel;
 
