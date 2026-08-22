9ffe79da053df841aea7e865a137b4a3b7e47ce4
theEvilReaper <theevilreaper@users.noreply.github.com>
2026-08-22 10:11:29 +0200

    chore(state): improve storage usage

--- lib/api/state/app_state.g.dart
+++ lib/api/state/app_state.g.dart
@@ -7,67 +7,6 @@ part of 'app_state.dart';
 // **************************************************************************
 
 _AppState _$AppStateFromJson(Map<String, dynamic> json) => _AppState(
-  items: json['items'] == null
-      ? const PaginatedResult<ItemModel>(
-          items: [],
-          totalItems: 0,
-          totalPages: 0,
-          currentPage: 1,
-          pageSize: 0,
-        )
-      : PaginatedResult<ItemModel>.fromJson(
-          json['items'] as Map<String, dynamic>,
-          (value) => ItemModel.fromJson(value as Map<String, dynamic>),
-        ),
-  notifications: json['notifications'] == null
-      ? const PaginatedResult<NotificationModel>(
-          items: [],
-          totalItems: 0,
-          totalPages: 0,
-          currentPage: 1,
-          pageSize: 0,
-        )
-      : PaginatedResult<NotificationModel>.fromJson(
-          json['notifications'] as Map<String, dynamic>,
-          (value) => NotificationModel.fromJson(value as Map<String, dynamic>),
-        ),
-  fonts: json['fonts'] == null
-      ? const PaginatedResult<FontModel>(
-          items: [],
-          totalItems: 0,
-          totalPages: 0,
-          currentPage: 1,
-          pageSize: 0,
-        )
-      : PaginatedResult<FontModel>.fromJson(
-          json['fonts'] as Map<String, dynamic>,
-          (value) => FontModel.fromJson(value as Map<String, dynamic>),
-        ),
-  attributes: json['attributes'] == null
-      ? const PaginatedResult<AttributeModel>(
-          items: [],
-          totalItems: 0,
-          totalPages: 0,
-          currentPage: 1,
-          pageSize: 0,
-        )
-      : PaginatedResult<AttributeModel>.fromJson(
-          json['attributes'] as Map<String, dynamic>,
-          (value) => AttributeModel.fromJson(value as Map<String, dynamic>),
-        ),
-  soundEvents: json['soundEvents'] == null
-      ? const PaginatedResult<SoundEventModel>(
-          items: [],
-          totalItems: 0,
-          totalPages: 0,
-          currentPage: 1,
-          pageSize: 0,
-        )
-      : PaginatedResult<SoundEventModel>.fromJson(
-          json['soundEvents'] as Map<String, dynamic>,
-          (value) => SoundEventModel.fromJson(value as Map<String, dynamic>),
-        ),
-  isLoadingRelease: json['isLoadingRelease'] as bool? ?? false,
   openNavigation: json['openNavigation'] as bool? ?? true,
   themeSettings: json['themeSettings'] == null
       ? const ThemeSettings(
@@ -78,54 +17,15 @@ _AppState _$AppStateFromJson(Map<String, dynamic> json) => _AppState(
           useSystemTheme: true,
         )
       : ThemeSettings.fromJson(json['themeSettings'] as Map<String, dynamic>),
-  selectedItem: json['selectedItem'] == null
-      ? null
-      : ItemModel.fromJson(json['selectedItem'] as Map<String, dynamic>),
-  selectedNotification: json['selectedNotification'] == null
-      ? null
-      : NotificationModel.fromJson(
-          json['selectedNotification'] as Map<String, dynamic>,
-        ),
-  selectedFont: json['selectedFont'] == null
-      ? null
-      : FontModel.fromJson(json['selectedFont'] as Map<String, dynamic>),
-  selectedAttribute: json['selectedAttribute'] == null
-      ? null
-      : AttributeModel.fromJson(
-          json['selectedAttribute'] as Map<String, dynamic>,
-        ),
-  selectedSoundEvent: json['selectedSoundEvent'] == null
-      ? null
-      : SoundEventModel.fromJson(
-          json['selectedSoundEvent'] as Map<String, dynamic>,
-        ),
-  releaseModel: json['releaseModel'] == null
-      ? null
-      : ReleaseModel.fromJson(json['releaseModel'] as Map<String, dynamic>),
-  isLoadingBranches: json['isLoadingBranches'] as bool? ?? false,
-  branches: (json['branches'] as List<dynamic>?)
-      ?.map((e) => e as String)
-      .toList(),
-  projects: (json['projects'] as List<dynamic>?)
+  projects:
+      (json['projects'] as List<dynamic>?)
           ?.map((e) => Project.fromJson(e as Map<String, dynamic>))
           .toList() ??
       const [],
-  selectedProject: json['selectedProject'] == null
-      ? null
-      : Project.fromJson(json['selectedProject'] as Map<String, dynamic>),
 );
 
 Map<String, dynamic> _$AppStateToJson(_AppState instance) => <String, dynamic>{
-  'items': instance.items.toJson((value) => value),
-  'notifications': instance.notifications.toJson((value) => value),
-  'fonts': instance.fonts.toJson((value) => value),
-  'attributes': instance.attributes.toJson((value) => value),
-  'soundEvents': instance.soundEvents.toJson((value) => value),
-  'isLoadingRelease': instance.isLoadingRelease,
   'openNavigation': instance.openNavigation,
   'themeSettings': instance.themeSettings,
-  'releaseModel': instance.releaseModel,
-  'isLoadingBranches': instance.isLoadingBranches,
-  'branches': instance.branches,
-  'projects': instance.projects.map((e) => e.toJson()).toList(),
+  'projects': instance.projects,
 };
